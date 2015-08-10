e18a141a99dc581e640785d657be793176ec300b
Standard IP <rhroyston@gmail.com>
2015-08-10 19:44:04 +0000

    My changes

--- includes/header.php
+++ includes/header.php
@@ -3,15 +3,16 @@
     <br>
     <h1 class="text-center">The Piano Pathway</h1>
     <h5 class="text-center">Music Center for Children & Adults</h5>
-    <br>
-    <h3 class="text-center">Baton Rouge&#39;s Premier Studio for Piano Lessons</h3>
     <div class="container">
         <div class="row">
             <div class="col-md-3"></div>
-            <h3 class="col-md-6 text-center"><small>private piano lessons &#124; partner piano lessons &#124; online piano lessons</small></h3>
+            <h3 class="col-md-6 text-center">private piano lessons &#124; partner piano lessons &#124; online piano lessons</h3>
             <div class="col-md-3"></div>
         </div>
     </div>
+    <br><br><br>
+    <h3 class="text-center">Baton Rouge&#39;s Premier Studio for Piano Lessons</h3>
+
     <div class="text-center">
         <a class="btn btn-primary" href="#" role="button">Link</a>
         <a class="btn btn-primary" href="#" role="button">Link</a>
