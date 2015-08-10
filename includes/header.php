c577397c2de78b2a0057704f39b5936f6e32066d
Standard IP <rhroyston@gmail.com>
2015-08-10 03:11:20 +0000

    My changes

--- includes/header.php
+++ includes/header.php
@@ -1,8 +1,7 @@
-<div class="jumbotron">
-  <div class="container-fluid banner">
+<div class="container-fluid banner">
     <h1>The Piano Pathway</h1>
-  </div>
 </div>
+
 <div class="container-fluid">
    <div class="row">
 	<img src="/images/header.png" class="img-responsive center-block">
