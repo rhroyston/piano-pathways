b9be4880ff5f2c29134955d71ac48bff6ac58877
Standard IP <rhroyston@gmail.com>
2015-08-10 18:29:13 +0000

    My changes

--- includes/header.php
+++ includes/header.php
@@ -2,8 +2,10 @@
     <br>
     <br>
     <h1 class="text-center">The Piano Pathway</h1>
-    <h3 class="text-center">Music Center for Children & Adults</h3>
+    <h5 class="text-center">Music Center for Children & Adults</h5>
     <br>
+    <h3>Baton Rouge&#39;s Premier Studio for Piano Lessons.</h3>
+    <p class="text-center">Welcome to Piano Pathways&#44; a team of professional piano teachers and Baton Rouge&#39;s premier piano studio. Piano Pathways offers a variety of lessons suitable for your individual learning style and unique goals &#45; private piano lessons&#44; partner piano lessons&#44; Recreational Music Making piano classes&#44; even online piano lessons&#33; We are excited to show you just how fun and rewarding learning to play the piano can be.</p>
     <div class="text-center">
         <a class="btn btn-primary" href="#" role="button">Link</a>
         <a class="btn btn-primary" href="#" role="button">Link</a>
