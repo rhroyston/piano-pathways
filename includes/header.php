469b4a0e0c5824175f61e4239de50cba73a6a684
Standard IP <rhroyston@gmail.com>
2015-08-10 01:38:41 +0000

    My changes

--- includes/header.php
+++ includes/header.php
@@ -1,7 +1,7 @@
-<div class="jumbotron">
-  <div class="container page-header">
+<div class="jumbotron banner">
+
     <h1>Hello, world!</h1>
     <p>...</p>
     <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
-  </div>
+
 </div>
