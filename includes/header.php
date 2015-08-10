65025767b107ac5d1e62fa7fdfca351e642884f7
Standard IP <rhroyston@gmail.com>
2015-08-10 19:33:44 +0000

    My changes

--- includes/header.php
+++ includes/header.php
@@ -7,9 +7,9 @@
     <h3 class="text-center">Baton Rouge&#39;s Premier Studio for Piano Lessons</h3>
     <div class="container">
         <div class="row">
-            <div class="col-md-3">.col-md-3</div>
-            <p class="col-md-6 text-center">Welcome to Piano Pathways&#44; a team of professional piano teachers and Baton Rouge&#39;s premier piano studio. Piano Pathways offers a variety of lessons suitable for your individual learning style and unique goals &#45; private piano lessons&#44; partner piano lessons&#44; Recreational Music Making piano classes&#44; even online piano lessons&#33; We are excited to show you just how fun and rewarding learning to play the piano can be.</p>
-            <div class="col-md-3">.col-md-3</div>
+            <div class="col-md-3"></div>
+            <h3 class="col-md-6 text-center"><small>Welcome to Piano Pathways&#44; a team of professional piano teachers and Baton Rouge&#39;s premier piano studio. Piano Pathways offers a variety of lessons suitable for your individual learning style and unique goals &#45; private piano lessons&#44; partner piano lessons&#44; Recreational Music Making piano classes&#44; even online piano lessons&#33; We are excited to show you just how fun and rewarding learning to play the piano can be.</small></h3>
+            <div class="col-md-3"></div>
         </div>
     </div>
     <div class="text-center">
