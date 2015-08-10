fa28f471aa8dffcc23c173d90a0edb1a5cc1e611
Standard IP <rhroyston@gmail.com>
2015-08-10 20:09:36 +0000

    My changes

--- includes/header.php
+++ includes/header.php
@@ -15,7 +15,7 @@
     <div class="container">
         <div class="row">
             <div class="col-md-2"></div>
-            <h1 class="col-md-8 text-center"><small>private piano lessons &#124; partner piano lessons &#124; online piano lessons</small></h1>
+            <h2 class="col-md-8 text-center">private piano lessons &#124; partner piano lessons &#124; online piano lessons</h2>
             <div class="col-md-2"></div>
         </div>
     </div>
