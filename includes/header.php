80e0cabcedf0091c120c1d98b01b08a1a1efcb35
Standard IP <rhroyston@gmail.com>
2015-08-10 03:06:32 +0000

    My changes

--- includes/header.php
+++ includes/header.php
@@ -1,3 +1,8 @@
+<div class="jumbotron">
+  <div class="container banner">
+    <h1>The Piano Pathway</h1>
+  </div>
+</div>
 <div class="container-fluid">
    <div class="row">
 	<img src="/images/header.png" class="img-responsive center-block">
--- index.php
+++ index.php
@@ -4,6 +4,6 @@
 
 <body>
   <?php include 'includes/header.php';?> 
-
+  <p>This is paragraph text.</p>
 </body>
 </html>
