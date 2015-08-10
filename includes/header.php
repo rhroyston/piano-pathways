0ed88aa29f9ccc9bf85f241fd3008c0eb1eeda0a
Standard IP <rhroyston@gmail.com>
2015-08-10 20:42:49 +0000

    My changes

--- includes/header.php
+++ includes/header.php
@@ -2,7 +2,7 @@
     <br>
     <br>
     <h1 class="text-center">The Piano Pathway</h1>
-    <h5 class="text-center">Music Center for Children & Adults</h5>
+    <h5 class="text-center">lessons &#124; recitals &#124; parties &#124; studio classes &#124; summer camps</h5>
     <br>
     <div class="text-center">
         <a class="btn btn-primary" href="#" role="button">Link</a>
@@ -11,12 +11,12 @@
     </div>
     <br>
     <br>
-    <h3 class="text-center offwhite">Customized accessible goal oriented learning for students of all ages&#44; at all skill levels.</h3>
+    <h3 class="text-center offwhite">Customized&#44; accessible&#44; goal oriented learning for students of all ages&#44; at all skill levels.</h3>
     <br>
     <div class="container">
         <div class="row">
             <div class="col-md-2"></div>
-            <h4 class="col-md-8 text-center">lessons &#124; recitals &#124; parties &#124; studio classes &#124; summer camps</h4>
+            <h4 class="col-md-8 text-center"></h4>
             <div class="col-md-2"></div>
         </div>
     </div>
