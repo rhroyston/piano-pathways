637f977339a4cb90c686db5ae30b414c04ebe64c
Standard IP <rhroyston@gmail.com>
2015-08-11 23:11:10 +0000

    My changes

--- includes/header.php
+++ includes/header.php
@@ -12,8 +12,8 @@
     <br>
     <br>
     <br>
-    <h2 class="text-center">Accessible&#44; Rewarding&#44; Goal Oriented Learning</h2>
-    <h3 class="text-center">For Students of All Ages&#44; At All Skill Levels</h3>
+    <h2 class="text-center"><b>Accessible&#44; Rewarding&#44; Goal Oriented Learning</b></h2>
+    <h3 class="text-center"><b>For Students of All Ages&#44; At All Skill Levels</b></h3>
     <br>
     <br>
     <br>
