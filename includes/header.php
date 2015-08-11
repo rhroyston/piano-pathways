832e0892410de1b0be2f9a7073cb9379dee0a188
Standard IP <rhroyston@gmail.com>
2015-08-11 18:22:25 +0000

    My changes

--- includes/header.php
+++ includes/header.php
@@ -1,8 +1,8 @@
 <div class="container-fluid banner">
     <br>
     <br>
-    <h1 class="text-center">The Piano Pathway</h1>
-    <h5 class="text-center">lessons &#124; recitals &#124; studio classes &#124; summer camps</h5>
+    <h1 class="text-center blue">The Piano Pathway</h1>
+    <h5 class="text-center blue"><b>lessons &#124; recitals &#124; studio classes &#124; summer camps</b></h5>
     <br>
     <div class="text-center">
         <a class="btn btn-primary" href="#" role="button">Link</a>
@@ -12,8 +12,8 @@
     <br>
     <br>
     <br>
-    <h2 class="text-center blue">Accessible&#44; Rewarding&#44; Goal Oriented Learning</h2>
-    <h3 class="text-center blue">For Students of All Ages&#44; At All Skill Levels</h3>
+    <h2 class="text-center">Accessible&#44; Rewarding&#44; Goal Oriented Learning</h2>
+    <h3 class="text-center">For Students of All Ages&#44; At All Skill Levels</h3>
     <br>
     <br>
     <br>
