a9c2a0050bcca9de89c17fe462b6e97dfec031d4
Standard IP <rhroyston@gmail.com>
2015-08-11 23:03:06 +0000

    My changes

--- index.php
+++ index.php
@@ -7,70 +7,6 @@
   <h1 class="text-center">Piano Instructors</h1>
 <br>
 <br>
-<div class="container">
-  <div class="row">
-      <div class="col-md-4">
-          <span><img src="/images/rebecca-bellelo.png" class="instructors"><h4>Rebecca Bellelo</h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span>
-      </div>
-      <div class="col-md-4">
-          <span><img src="/images/sarah-stoltzfus-johnson.png" class="instructors"><h4>Sarah Stoltzfus Johnson</h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span>
-      </div>
-      <div class="col-md-4">
-          <span><img src="/images/emily-elbourne-ball.png" class="instructors"><h4>Emily Elbourne Ball</h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span>
-      </div>
-  </div>
-  <br> 
-  <br>
-  <div class="row">
-      <div class="col-md-2">
-      </div>
-      <div class="col-md-4">
-          <span><img src="/images/melanie-mallard.png" class="instructors"><h4>Melanie Mallard</h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span>
-      </div>
-      <div class="col-md-4">
-          <span><img src="/images/joshua-medrano.png" class="instructors"><h4>Joshua Medrano</h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span>
-      </div>
-      <div class="col-md-2">
-      </div>
-  </div>
-</div>
-<br>
-<br>
-<br>
-<br>
-<div class="container what">
-<br>
-<br>
-<br>
-<br>
-<h1 class="text-center">Which Path Is Right For You&#63;</h1>
-<br>
-<br>
-<div class="row">
-  <div class="col-md-2">
-  </div>
-  <div class="col-md-4">
-      <h3 class="text-center">Children&#38; Teen Piano Instruction</h3>
-      <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-  </div>
-  <div class="col-md-4">
-      <h3 class="text-center">Adult Piano Instruction</h3>
-      <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-  </div>
-  <div class="col-md-2">
-  </div>
-</div>
-<br>
-<br>
-<br>
-<br>
-</div>
-
-<div class="container-fluid">
-   <div class="row">
-	<img src="/images/pianoman.png" class="img-responsive center-block">
-   </div>
-</div>
 
 <div class="container why">
 <br>
@@ -98,6 +34,74 @@
 <br>
 <br>
 <br>
+</div>
+
+<br>
+<br>
+<br>
+<br>
+<div class="container what">
+<br>
+<br>
+<br>
+<br>
+<h1 class="text-center">Which Path Is Right For You&#63;</h1>
+<br>
+<br>
+<div class="row">
+  <div class="col-md-1">
+  </div>
+  <div class="col-md-5">
+      <h3 class="text-center">Children&#38; Teen Piano Instruction</h3>
+      <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  </div>
+  <div class="col-md-5">
+      <h3 class="text-center">Adult Piano Instruction</h3>
+      <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  </div>
+  <div class="col-md-1">
+  </div>
+</div>
+<br>
+<br>
+<br>
+<br>
+</div>
+
+<div class="container-fluid">
+   <div class="row">
+	<img src="/images/pianoman.png" class="img-responsive center-block">
+   </div>
+</div>
+
+
+
+<div class="container">
+  <div class="row">
+      <div class="col-md-4">
+          <span><img src="/images/rebecca-bellelo.png" class="instructors"><h4>Rebecca Bellelo</h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span>
+      </div>
+      <div class="col-md-4">
+          <span><img src="/images/sarah-stoltzfus-johnson.png" class="instructors"><h4>Sarah Stoltzfus Johnson</h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span>
+      </div>
+      <div class="col-md-4">
+          <span><img src="/images/emily-elbourne-ball.png" class="instructors"><h4>Emily Elbourne Ball</h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span>
+      </div>
+  </div>
+  <br> 
+  <br>
+  <div class="row">
+      <div class="col-md-2">
+      </div>
+      <div class="col-md-4">
+          <span><img src="/images/melanie-mallard.png" class="instructors"><h4>Melanie Mallard</h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span>
+      </div>
+      <div class="col-md-4">
+          <span><img src="/images/joshua-medrano.png" class="instructors"><h4>Joshua Medrano</h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span>
+      </div>
+      <div class="col-md-2">
+      </div>
+  </div>
 </div>
 
 </body>
