4ae7f14621e6cccd033646b7046a80a59a2ccb61
Standard IP <rhroyston@gmail.com>
2015-08-11 18:46:21 +0000

    My changes

--- index.php
+++ index.php
@@ -45,13 +45,17 @@
 <br>
 <br>
 <div class="row">
-  <div class="col-md-6">
+  <div class="col-md-2">
+  </div>
+  <div class="col-md-4">
       <h3 class="text-center">Children and Teen Piano Instruction</h3>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
   </div>
-  <div class="col-md-6">.col-md-6
+  <div class="col-md-4">
       <h3 class="text-center">Adult Piano Instruction</h3>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  </div>
+  <div class="col-md-2">
   </div>
 </div>
 <br>
