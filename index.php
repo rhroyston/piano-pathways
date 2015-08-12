8901255e096a9df33bdf9bd90082076d7738e67a
Standard IP <rhroyston@gmail.com>
2015-08-12 02:38:42 +0000

    My changes

--- index.php
+++ index.php
@@ -77,36 +77,36 @@
 </div>
 
 <div class="container instructors">
-<br>
-<br>
-<h1 class="text-center">Piano Instructors</h1>
-<br>
-<br>
-  <div class="row">
-      <div class="col-md-4">
-          <span><img src="/images/rebecca-bellelo.png" class="instructors"><h4>Rebecca Bellelo</h4><h6>Studio Founder&#44; Owner&#44; and Piano Faculty</h6>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span>
-      </div>
-      <div class="col-md-4">
-          <span><img src="/images/sarah-stoltzfus-johnson.png" class="instructors"><h4>Sarah Stoltzfus Johnson</h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span>
-      </div>
-      <div class="col-md-4">
-          <span><img src="/images/emily-elbourne-ball.png" class="instructors"><h4>Emily Elbourne Ball</h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span>
-      </div>
-  </div>
-  <br> 
   <br>
-  <div class="row">
-      <div class="col-md-2">
-      </div>
-      <div class="col-md-4">
-          <span><img src="/images/melanie-mallard.png" class="instructors"><h4>Melanie Mallard</h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span>
-      </div>
-      <div class="col-md-4">
-          <span><img src="/images/joshua-medrano.png" class="instructors"><h4>Joshua Medrano</h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span>
-      </div>
-      <div class="col-md-2">
-      </div>
-  </div>
+  <br>
+  <h1 class="text-center">Piano Instructors</h1>
+  <br>
+  <br>
+    <div class="row">
+        <div class="col-md-4">
+            <span><img src="/images/rebecca-bellelo.png" class="instructors"><h4>Rebecca Bellelo</h4><h6>Studio Founder&#44; Owner&#44; and Piano Faculty</h6>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span>
+        </div>
+        <div class="col-md-4">
+            <span><img src="/images/sarah-stoltzfus-johnson.png" class="instructors"><h4>Sarah Stoltzfus Johnson</h4><h6>Piano Faculty</h6>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span>
+        </div>
+        <div class="col-md-4">
+            <span><img src="/images/emily-elbourne-ball.png" class="instructors"><h4>Emily Elbourne Ball</h4><h6>Piano Faculty</h6>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span>
+        </div>
+    </div>
+    <br> 
+    <br>
+    <div class="row">
+        <div class="col-md-2">
+        </div>
+        <div class="col-md-4">
+            <span><img src="/images/melanie-mallard.png" class="instructors"><h4>Melanie Mallard</h4><h6>Piano Faculty</h6>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span>
+        </div>
+        <div class="col-md-4">
+            <span><img src="/images/joshua-medrano.png" class="instructors"><h4>Joshua Medrano</h4><h6>Piano Faculty</h6>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span>
+        </div>
+        <div class="col-md-2">
+        </div>
+    </div>
 </div>
 
 </body>
