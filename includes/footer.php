964a74483093dd72ddac9c17e3925092bcad9777
Standard IP <rhroyston@gmail.com>
2015-08-12 21:57:47 +0000

    My changes

--- includes/footer.php
+++ includes/footer.php
@@ -69,6 +69,6 @@
     <br>
     <br>
     <br>
-    <h6 class="text-center"><small>&#169; Copyright 2011&#45;2016 &#124; Piano Pathways &#124; 9270 Siegen Lane #304 &#124; Baton Rouge&#44; LA 70810</small></h6>
+    <h6 class="text-right"><small>&#169; Copyright 2011&#45;2016 &#124; Piano Pathways &#124; 9270 Siegen Lane #304 &#124; Baton Rouge&#44; LA 70810</small></h6>
   </div>
 </div>
