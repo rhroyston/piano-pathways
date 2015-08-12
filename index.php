4957cfc15d232f4a1867678ecdb80ed0d909d8b7
Standard IP <rhroyston@gmail.com>
2015-08-12 21:37:31 +0000

    My changes

--- index.php
+++ index.php
@@ -11,25 +11,7 @@
    </div>
 </div>
 
-<div class="container why">
-  <br>
-  <div class="row">
-    <div class="col-md-1">
-    </div>
-    <div class="col-md-5">
-        <h3 class="text-center">Children&#38; Teen Piano Instruction</h3>
-        <p class="text-center">Music is exciting at Piano Pathways&#33; Students learn how to read music&#44; play duets and ensembles&#44; improvise&#44; music theory and history&#44; and appreciate all types of music. Students play standard classical music&#44; Christmas music&#44; Halloween pieces&#44; pop music&#44; jazz&#44; even hymns&#44; while accessing technology in the learning process.</p>
-    </div>
-    <div class="col-md-5">
-        <h3 class="text-center">Adult Piano Instruction</h3>
-        <p class="text-center">It is never too late to learn to play piano&#33; In adult lessons&#44; the student guides the lesson while working alongside the teacher to build an individualized program. Whether you are just starting out or have taken before as a child and need some refreshing&#44; you will find the joy of playing piano at Piano Pathways.</p>
-    </div>
-    <div class="col-md-1">
-    </div>
-  </div>
-  <br>
-  <br>
-</div>
+<?php include 'includes/why.php';?>
 
 <div class="container what">
   <br>
