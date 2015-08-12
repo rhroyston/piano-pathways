d5546b4e9914a034856d018baa8a1c1fe59517a0
Standard IP <rhroyston@gmail.com>
2015-08-12 01:59:16 +0000

    My changes

--- index.php
+++ index.php
@@ -12,71 +12,72 @@
 </div>
 
 <div class="container why">
-<br>
-<div class="row">
-  <div class="col-md-1">
+  <br>
+  <div class="row">
+    <div class="col-md-1">
+    </div>
+    <div class="col-md-5">
+        <h3 class="text-center">Children&#38; Teen Piano Instruction</h3>
+        <p class="text-center">Music is exciting at Piano Pathways&#33; Students learn how to read music&#44; play duets and ensembles&#44; improvise&#44; music theory and history&#44; and appreciate all types of music. Students play standard classical music&#44; Christmas music&#44; Halloween pieces&#44; pop music&#44; jazz&#44; even hymns&#44; while accessing technology in the learning process.</p>
+    </div>
+    <div class="col-md-5">
+        <h3 class="text-center">Adult Piano Instruction</h3>
+        <p class="text-center">It is never too late to learn to play piano&#33; In adult lessons&#44; the student guides the lesson while working alongside the teacher to build an individualized program. Whether you are just starting out or have taken before as a child and need some refreshing&#44; you will find the joy of playing piano at Piano Pathways.</p>
+    </div>
+    <div class="col-md-1">
+    </div>
   </div>
-  <div class="col-md-5">
-      <h3 class="text-center">Children&#38; Teen Piano Instruction</h3>
-      <p class="text-center">Music is exciting at Piano Pathways&#33; Students learn how to read music&#44; play duets and ensembles&#44; improvise&#44; music theory and history&#44; and appreciate all types of music. Students play standard classical music&#44; Christmas music&#44; Halloween pieces&#44; pop music&#44; jazz&#44; even hymns&#44; while accessing technology in the learning process.</p>
+  <br>
+  <div class="row">
+    <div class="col-md-1">
+    </div>
+    <div class="col-md-10">
+      <h1 class="text-center blue">Accredited&#38; Recognized
+      </h1>
+    </div>
+    <div class="col-md-1">
+    </div>
   </div>
-  <div class="col-md-5">
-      <h3 class="text-center">Adult Piano Instruction</h3>
-      <p class="text-center">It is never too late to learn to play piano&#33; In adult lessons&#44; the student guides the lesson while working alongside the teacher to build an individualized program. Whether you are just starting out or have taken before as a child and need some refreshing&#44; you will find the joy of playing piano at Piano Pathways.</p>
-  </div>
-  <div class="col-md-1">
-  </div>
-</div>
-<br>
-<div class="row">
-  <div class="col-md-1">
-  </div>
-  <div class="col-md-10">
-    <h1 class="text-center blue">Accredited&#38; Recognized
-    </h1>
-  </div>
-  <div class="col-md-1">
-  </div>
-</div>
-<br>
+  <br>
     <div class="container-fluid">
        <div class="row">
     	<img src="/images/credentialsbanner.png" class="img-responsive center-block">
        </div>
     </div>
-<br>
-<br>
+  <br>
+  <br>
+  <br>
 </div>
-<br>
-<br>
+
 <div class="container what">
-<br>
-<br>
-<br>
-<br>
-<h1 class="text-center">Why Choose Us&#63;</h1>
-<br>
-<br>
-<div class="row">
-  <div class="col-md-1">
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <h1 class="text-center">Why Choose Us&#63;</h1>
+  <br>
+  <br>
+  <div class="row">
+    <div class="col-md-1">
+    </div>
+    <div class="col-md-5">
+        <h3 class="text-center">Participation</h3>
+        <p>Students are encouraged to participate in local festivals&#44; adjudications&#44; studio classes&#44; and recitals throughout the year to showcase their current work in lessons. Students have participated in and placed in the LSU Sonatina and Bach Festival&#44; the BRMTA Tournament&#44; Baton Rouge District Rally&#44; and the Torgrimson&#47;Swanzey State Competition.
+        </p>
+    </div>
+    <div class="col-md-5">
+        <h3 class="text-center">Success</h3>
+        <p>Our top priority is student success&#44; and we get results by effective teaching methods&#44; holding students to high expectations&#44; structured&#38; organized lessons&#44; long& short&#45;term goals&#44; formal&#38; informal assessments&#44; and consistent teaching approaches.
+        </p>
+    </div>
+    <div class="col-md-1">
+    </div>
   </div>
-  <div class="col-md-5">
-      <h3 class="text-center">Engage</h3>
-      <p class="text-center">Students are encouraged to participate in local festivals&#44; adjudications&#44; studio classes&#44; and recitals throughout the year to showcase their current work in lessons. Students have participated in and placed in the LSU Sonatina and Bach Festival&#44; the BRMTA Tournament&#44; Baton Rouge District Rally&#44; and the Torgrimson&#47;Swanzey State Competition.
-      </p>
-  </div>
-  <div class="col-md-5">
-      <h3 class="text-center">Success</h3>
-      <p class="text-center">Our top priority is student success&#44; and we get results by effective teaching methods&#44; holding students to high expectations&#44; structured&#38; organized lessons&#44; long& short&#45;term goals&#44; formal&#38; informal assessments&#44; and consistent teaching approaches.
-      </p>
-  </div>
-  <div class="col-md-1">
-  </div>
-</div>
-<br>
-<br>
-<br>
-<br>
+  <br>
+  <br>
+  <br>
+  <br>
 </div>
 
 <h1 class="text-center">Piano Instructors</h1>
