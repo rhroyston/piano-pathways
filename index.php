4fc7349b6d4679e5b5134cac858dc81f7239b50c
Standard IP <rhroyston@gmail.com>
2015-08-12 03:08:51 +0000

    My changes

--- index.php
+++ index.php
@@ -109,5 +109,9 @@
     </div>
 </div>
 
+<div class="container map-frame">
+  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3442.424955469731!2d-91.07516620000007!3d30.367288699999992!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8626a53e88fc2d09%3A0xda2e043d4fe40d5d!2s9270+Siegen+Ln+%23304%2C+Baton+Rouge%2C+LA+70810!5e0!3m2!1sen!2sus!4v1439348379351" width="1200" height="400" frameborder="0" style="border:0" allowfullscreen></iframe>
+</div>
+
 </body>
 </html>
