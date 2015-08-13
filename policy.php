13a15e95998b44dcaf47100b43c80e6d0ed3e890
Standard IP <rhroyston@gmail.com>
2015-08-13 17:48:08 +0000

    My changes

---
+++ policy.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+  <?php include 'includes/head.php';?>
+  <body>
+    <?php include 'includes/header.php';?>
+    <div class="container-fluid">
+        <div class="row">
+          <img src="/images/pianoman.png" class="img-responsive center-block">
+        </div>
+    </div>
+
+
+<h1 class="text-center"><small>Studio Policy</small></h1>
+<br>
+<h2>Overview</h2>
+<p>The team of professional piano teachers at Piano Pathways believe that music is for everyone and everyone can be musical. Each class is a structured&#44; musical experience. Developing a life&#45;long musical appreciation&#44; participation&#44; and true musical independence are goals that Piano Pathways instructors have for every student.
+</p>
+
+<p>This policy serves as an agreement between parents&#44; students&#44; and teachers. Parents&#44; students&#44; and teachers each have expectations and responsibilities in order to make piano lessons a successful experience.
+</p>
+Students&#39; Responsibilities
+
+Please play your instrument at home every week between lessons. You learn to play the piano by playing the piano&#33;
+Complete all assignments from your teacher. This includes any written theory&#44; scale practice&#44; and sight reading work&#33;&#41;
+Know what your goals are–   your practice goals&#44; performance goals&#44; and goals for long&#45;term progress.
+
+Parents&#39; Responsibilities
+
+Please encourage your child to play the piano at home. Be involved with this activity and give it the same value as you do other activities in which your child is involved. This is an endeavor that requires consistency and a long&#45;term commitment.
+If questions arise or practicing seems a struggle at home&#44; please communicate with the teacher. We can be your best resource and guide to answer any questions&#44; and we are glad to talk with you.
+Be timely&#45; this includes paying tuition on time and arriving to lessons on time.
+
+Teachers&#39; Responsibilities
+
+Structure each lesson to be a positive&#44; music&#45;filled experience that centers around what is best for the individual student.
+Plan long&#45;term and short&#45;term goals to help the student reach his or her full musical capabilities&#44; including repertoire selection&#44; theory&#44; festivals&#44; recitals&#44; and lesson format.
+Communicate openly with the student about his or her progress&#44; goals&#44; practice strategies&#44; and other related information in order that the student becomes musically independent.
+ 
+
+Scheduling &#38; Attendance
+<p>The studio follows a 16&#45;week fall semester &#40;August&#45;December&#41;&#44; a 16&#45;week spring semester &#40;January&#45;May&#41;&#44; and a 6&#45;week summer semester &#40;June&#45;July&#41;. Students are required to enroll in all semesters&#44; as the summer semester combined with a yearly registration fee will guarantee his lesson space in the studio for the upcoming school year. The studio complies with the EBRPSS school year calendar.
+</p>
+<p>Each student will have an exclusive weekly lesson time&#44; based on the most appropriate time determined by the teacher&#39;s&#44; parent&#39;s&#44; and child&#39;s schedule. Once the final lesson schedule is in place for the current semester&#44; lesson times&#44; days&#44; and length of time may not be moved or altered until the next semester. Lesson appointments can be changed at the beginning of each semester. A yearly registration fee during the summer semester will reserve each student&#39;s lesson time for the fall semester and is non&#45;refundable should the student decide not to re&#45;enroll after summer. Students interested in joining the student during the spring semester or summer semester will be offered available times in the teaching schedule and are required to pay a reduced yearly registration fee at the time of joining the studio.
+</p>
+Make&#45;Up Lessons
+<p>There will be no credits&#44; refunds&#44; or make&#45;ups for any missed lessons or classes. Please read this detailed explanation for a helpful and informative description about make&#45;up lessons from an economist&#47;parent&#39;s point of view. If the student has a serious conflict with the lesson appointment for one week &#40;which does not include conflicts due to other activities&#44; play dates&#44; or family trips&#41; and the instructor is notified at least 24 hours prior to the missed appointment&#44; it may be possible to swap or substitute lessons with another student&#39;s appointment. This swap must take place within that week of the missed lesson.
+</p>
+<p>At the teacher&#39;s discretion&#44; there may be one day after the semester recital that is designated as a performance class that students can attend to substitute for 1 lesson absence. Lessons missed by the instructor will be made&#45;up or credited. In the case of an extreme family emergency&#44; illness&#44; or death&#44; the instructor is allowed to miss a lesson each semester without a make&#45;up lesson given.
+</p>
+<p>Online lessons that are unable to meet due to any technological difficulties will be made&#45;up.
+</p>
+Practicing
+<p>The student&#44; parents&#44; and teacher will discuss an appropriate practice strategy in order to ensure the most successful piano experience possible&#44; based on the student&#39;s and families&#39; goals in taking lessons. The amount of practice time directly affects progress and should be established into the child&#39;s daily routine. During each lesson&#44; we will explore problem&#45;solving strategies and refine practice skills in order to practice smarter.
+</p>
+Waiting Room
+<p>In order to minimize distractions&#44; parents are strongly encouraged to utilize only the office space during lesson times.
+</p>
+Performances and Recitals
+<p>Adjudications and public performances are an important part of learning to play piano. Students are strongly encouraged to participate in the Winter Recital and Spring Recital at the end of each semester. Students are also encouraged to participate in at least one festival or competition throughout the school year and can be decided based on the teacher&#39;s recommendations for each student.
+</p>
+Instrument
+<p>Students must have an instrument at home for daily practice. Pianos should be in good working condition and tuned regularly. A digital keyboard with weighted&#44; 88&#45;keys&#44; a bench&#44; and a pedal&#44; is acceptable for home practice.
+</p>
+<p>Students taking online lessons must have a digital keyboard or MIDI&#45;capable piano. Ask Rebecca what options you may have if you are interested in purchasing an acoustic piano with MIDI&#45;capabilities.
+</p>
+Lesson Termination
+<p>Each student&#39;s registration enrollment is a commitment to lessons and payment for services for the entire semester&#44; January&#45;May&#44; June&#45;July&#44; or August&#45;December. A 30&#45;day notice is requested in the event that the student can no longer participate in piano lessons through the remainder of the semester. Tuition fees are still applicable for the remaining semester&#44; and any tuition fees for remaining lessons will not be refunded or credited. The instructors reserve the rights to terminate the lesson contract with any student due to lack of practice commitment&#44; excessive absences with or without notifications&#44; failure to pay tuition&#44; or if she professional believes that the student will be better served by another piano teacher.
+</p>
+
+<p>Please note these policies are put in place to protect the interest of the instructors and piano students. This studio policy serves as a formal document for service of and payment for piano instruction. Registering for lessons indicates that the student and parents have read&#44; agree to&#44; and will comply with all policies outlined.
+</p>
+
+
+
+
+
+
+
+
+
+    <?php include 'includes/footer.php';?>
+    <?php include 'includes/resources.php';?>
+  </body>
+</html>
