05713a948164752545e3b896916b1e8aca52cf63
Standard IP <rhroyston@gmail.com>
2015-08-13 19:30:44 +0000

    My changes

--- includes/banner.php
+++ includes/banner.php
@@ -1,5 +1,4 @@
 <div class="container-fluid header">
-    <h2 class="text-center">Accessible&#44; Rewarding&#44; Goal Oriented Learning</h2>
     <h3 class="text-center">Fall Enrollment Still Open&#33;</h3>
     <br>
     <br>
--- includes/footer.php
+++ includes/footer.php
@@ -11,44 +11,44 @@
             <a href="policy"><i class="fa fa-info-circle"></i> Studio Policy</a><br>
             <a href="#schedulingModal" data-toggle="modal">Scheduling <i class="fa fa-angle-double-right"></i></a><br>
             <a href="#tuitionModal" data-toggle="modal">Tuition <i class="fa fa-angle-double-right"></i></a><br>
-            <a href="http://www.musiclearningcommunity.com">Make A Payment <i class="fa fa-external-link"></i></a><br>
+            <a href="http://www.musiclearningcommunity.com" target="blank">Make A Payment <i class="fa fa-external-link"></i></a><br>
             <a href="http://www.encoremusiclessons.com/images/infographic/infographic-piano-lessons-are-good-for-you-and-your-brain.png" target="blank">Benefits of Piano Lessons <i class="fa fa-external-link"></i></a><br>
             <a href="#wheretuitiongoesModal" data-toggle="modal">Where Does Tuition Go&#63; <i class="fa fa-angle-double-right"></i></a><br>
         </div>
         
         <div class="col-md-4">
           <h3 class="white">Music Theory Websites</h3>
-          <a href="http://www.musiclearningcommunity.com">MusicLearningCommunity.com <i class="fa fa-external-link"></i></a><br>
-          <a href="http://musictheoryblog.blogspot.com/2008/02/basic-piano-chords.html">Blogspot Music Theory Blog <i class="fa fa-external-link"></i></a><br>
-          <a href="http://www.teoria.com/exercises/index.php">Teoria <i class="fa fa-external-link"></i></a><br>
-          <a href="http://www.musictheory.net">musictheory.net <i class="fa fa-external-link"></i></a><br>
-          <a href="http://www.emusictheory.com/practice.html">eMusicTheory <i class="fa fa-external-link"></i></a><br>
+          <a href="http://www.musiclearningcommunity.com" target="blank">MusicLearningCommunity.com <i class="fa fa-external-link"></i></a><br>
+          <a href="http://musictheoryblog.blogspot.com/2008/02/basic-piano-chords.html" target="blank">Blogspot Music Theory Blog <i class="fa fa-external-link"></i></a><br>
+          <a href="http://www.teoria.com/exercises/index.php" target="blank">Teoria <i class="fa fa-external-link"></i></a><br>
+          <a href="http://www.musictheory.net" target="blank">musictheory.net <i class="fa fa-external-link"></i></a><br>
+          <a href="http://www.emusictheory.com/practice.html" target="blank">eMusicTheory <i class="fa fa-external-link"></i></a><br>
           <br>
           <h3 class="white">Music History Websites</h3>
-          <a href="http://www.classicsforkids.com">Classics for Kids <i class="fa fa-external-link"></i></a><br>
+          <a href="http://www.classicsforkids.com" target="blank">Classics for Kids <i class="fa fa-external-link"></i></a><br>
           <br>
           <h3 class="white">Professional Music Organizations</h3>
           <a href="http://www.brmta.org" target="blank">Baton Rouge Music Teachers Association <i class="fa fa-external-link"></i></a><br>
-          <a href="http://www.lmta.org">Louisiana Music Teachers Association <i class="fa fa-external-link"></i></a><br>
-          <a href="http://www.mtna.org">Music Teachers National Association <i class="fa fa-external-link"></i></a><br>
-          <a href="http://www.menc.org">National Association for Music Education <i class="fa fa-external-link"></i></a><br>
-          <a href="http://www.music.lsu.edu">LSU School of Music <i class="fa fa-external-link"></i></a><br>
+          <a href="http://www.lmta.org" target="blank">Louisiana Music Teachers Association <i class="fa fa-external-link"></i></a><br>
+          <a href="http://www.mtna.org" target="blank">Music Teachers National Association <i class="fa fa-external-link"></i></a><br>
+          <a href="http://www.menc.org" target="blank">National Association for Music Education <i class="fa fa-external-link"></i></a><br>
+          <a href="http://www.music.lsu.edu" target="blank">LSU School of Music <i class="fa fa-external-link"></i></a><br>
         </div>
         
         <div class="col-md-4">
           <h3 class="white">Local Music Stores</h3>
-          <a href="http://www.oneillsmusic.com/">O&#39;Neill&#39;s Music <i class="fa fa-external-link"></i></a><br>
-          <a href="http://zeaglermusic.com/">Zeagler&#39;s Music <i class="fa fa-external-link"></i></a><br>
+          <a href="http://www.oneillsmusic.com/" target="blank">O&#39;Neill&#39;s Music <i class="fa fa-external-link"></i></a><br>
+          <a href="http://zeaglermusic.com/" target="blank">Zeagler&#39;s Music <i class="fa fa-external-link"></i></a><br>
           <h3 class="white">Online Music Stores</h3>
-          <a href="http://www.sheetmusicplus.com/">Sheet Music Plus <i class="fa fa-external-link"></i></a><br>
-          <a href="http://www.musicnotes.com/">musicnotes.com <i class="fa fa-external-link"></i></a><br>
-          <a href="http://www.jwpepper.com/sheet-music/welcome.jsp">JW Pepper <i class="fa fa-external-link"></i></a><br>
+          <a href="http://www.sheetmusicplus.com/" target="blank">Sheet Music Plus <i class="fa fa-external-link"></i></a><br>
+          <a href="http://www.musicnotes.com/" target="blank">musicnotes.com <i class="fa fa-external-link"></i></a><br>
+          <a href="http://www.jwpepper.com/sheet-music/welcome.jsp" target="blank">JW Pepper <i class="fa fa-external-link"></i></a><br>
           <h3 class="white">Digital Sheet Music</h3>
-          <a href="http://www.musicnotes.com/">musicnotes.com <i class="fa fa-external-link"></i></a><br>        
+          <a href="http://www.musicnotes.com/" target="blank">musicnotes.com <i class="fa fa-external-link"></i></a><br>        
           <h3 class="white">Metronome</h3>
-          <a href="https://www.metronomeonline.com/">Metronome Online <i class="fa fa-external-link"></i></a><br>        
+          <a href="https://www.metronomeonline.com/" target="blank">Metronome Online <i class="fa fa-external-link"></i></a><br>        
           <h3 class="white">Staff Paper</h3>
-          <a href="http://www.blanksheetmusic.net/">blanksheetmusic.net <i class="fa fa-external-link"></i></a><br>        
+          <a href="http://www.blanksheetmusic.net/" target="blank">blanksheetmusic.net <i class="fa fa-external-link"></i></a><br>        
         </div>
     </div>
     <br>
