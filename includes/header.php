d406bdbe7aa12dcdf6eb1976dfce65b2e94a61ab
Standard IP <rhroyston@gmail.com>
2015-08-13 18:47:51 +0000

    My changes

--- includes/header.php
+++ includes/header.php
@@ -8,9 +8,9 @@
     </h6>
     <div class="row">
       <img src="/images/logo.png" class="img-responsive center-block">
-      <h6 class="text-center">9270 Siegen Lane #304 Baton Rouge, LA 70810 &#40;225&#41; 767&#45;0030</h6>
+      <h6 class="text-center adjustup">9270 Siegen Lane #304 Baton Rouge, LA 70810 &#40;225&#41; 767&#45;0030</h6>
     </div>
-    <h6 class="text-center blue">BATON ROUGE&#39;S MUSIC CENTER FOR CHILDREN&#38; ADULTS</h6>
+    <h5 class="text-center blue">BATON ROUGE&#39;S MUSIC CENTER FOR CHILDREN&#38; ADULTS</h5>
     <h5 class="text-center blue">lessons &#124; recitals &#124; studio classes &#124; summer camps</h5>
     <br>
     <div class="text-center">
