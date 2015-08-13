712feca82adc798365c76ebae21f92417d877121
Standard IP <rhroyston@gmail.com>
2015-08-13 19:04:39 +0000

    My changes

--- includes/banner.php
+++ includes/banner.php
@@ -1,6 +1,6 @@
 <div class="container-fluid header">
-    <h2 class="text-center"><b>Accessible&#44; Rewarding&#44; Goal Oriented Learning</b></h2>
-    <h3 class="text-center"><b>For Students of All Ages&#44; At All Skill Levels</b></h3>
+    <h2 class="text-center">Accessible&#44; Rewarding&#44; Goal Oriented Learning</h2>
+    <h3 class="text-center">Fall Enrollment Still Open&#33;</h3>
     <br>
     <br>
 </div>
