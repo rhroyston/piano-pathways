4a689d601697f05023f0ff34b7930154c3318f57
Standard IP <rhroyston@gmail.com>
2015-08-13 03:54:43 +0000

    My changes

--- includes/footer.php
+++ includes/footer.php
@@ -7,7 +7,7 @@
     <div class="row">
         <div class="col-md-4">
             <h3 class="white">Piano Pathways Resources</h3>
-            <a href="#faqModal"><i class="fa fa-question-circle"></i> FAQ&#39;s <i class="fa fa-angle-double-right"></i></a><br>
+            <a href="#faqModal" data-toggle="modal"><i class="fa fa-question-circle"></i> FAQ&#39;s <i class="fa fa-angle-double-right"></i></a><br>
             <a href="http://www.musiclearningcommunity.com"><i class="fa fa-info-circle"></i> Studio Policy</a><br>
             <a href="#schedulingModal" data-toggle="modal">Scheduling <i class="fa fa-angle-double-right"></i></a><br>
             <a href="#tuitionModal" data-toggle="modal">Tuition <i class="fa fa-angle-double-right"></i></a><br>
