6febf820c4c5a2bce9d253625e9e178353a461b9
Standard IP <rhroyston@gmail.com>
2015-08-13 17:40:01 +0000

    My changes

--- includes/header.php
+++ includes/header.php
@@ -1,4 +1,4 @@
-<div class="container-fluid banner">
+<div class="container-fluid header">
     <h6 class="text-right">
         <a href="https://twitter.com/PianoPathwaysBR"><i class="fa fa-twitter"></i></a>&nbsp;&nbsp;
         <a href="https://www.facebook.com/PianoPathwaysStudio"><i class="fa fa-facebook-square"></i></a>&nbsp;&nbsp;
@@ -18,10 +18,6 @@
         <a class="btn btn-primary" href="https://www.google.com/maps/place/9270+Siegen+Ln+%23304,+Baton+Rouge,+LA+70810/@30.3672887,-91.0751662,17z/data=!3m1!4b1!4m2!3m1!1s0x8626a53e88fc2d09:0xda2e043d4fe40d5d" target="blank" role="button"><i class="fa fa-map-marker"></i> Directions</a>
     </div>
     <br>
-    <br>
-    <br>
-    <h2 class="text-center"><b>Accessible&#44; Rewarding&#44; Goal Oriented Learning</b></h2>
-    <h3 class="text-center"><b>For Students of All Ages&#44; At All Skill Levels</b></h3>
     <br>
     <br>
 </div>
--- index.php
+++ index.php
@@ -1,22 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
   <?php include 'includes/head.php';?>
-  
   <body>
     <?php include 'includes/header.php';?>
-  
+    <?php include 'includes/banner.php';?>
     <div class="container-fluid">
-       <div class="row">
-    	<img src="/images/pianoman.png" class="img-responsive center-block">
-       </div>
+        <div class="row">
+          <img src="/images/pianoman.png" class="img-responsive center-block">
+        </div>
     </div>
-    
     <?php include 'includes/why.php';?>
-    
     <?php include 'includes/instructors.php';?>
-    
     <?php include 'includes/footer.php';?>
     <?php include 'includes/resources.php';?>
-  
   </body>
 </html>
