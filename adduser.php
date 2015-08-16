b74de3a1060f80ca1aeedaadac6e521bcb3ceb99
Standard IP <rhroyston@gmail.com>
2015-08-16 03:10:12 +0000

    My changes

--- adduser.php
+++ adduser.php
@@ -16,26 +16,26 @@ $_SESSION['form_token'] = $form_token;
 </head>
 
 <body>
-<h2>Add user</h2>
-<form action="includes/adduser_submit" method="post">
-<fieldset>
-<p>
-<label for="phpro_username">Username</label>
-<input type="text" id="phpro_username" name="phpro_username" value="" maxlength="20" />
-</p>
-<p>
-<label for="phpro_email">Email</label>
-<input type="text" id="phpro_email" name="phpro_email" value="" maxlength="40" />
-</p>
-<p>
-<label for="phpro_password">Password</label>
-<input type="text" id="phpro_password" name="phpro_password" value="" maxlength="20" />
-</p>
-<p>
-<input type="hidden" name="form_token" value="<?php echo $form_token; ?>" />
-<input type="submit" value="&rarr; Login" />
-</p>
-</fieldset>
-</form>
+    <h2>Add user</h2>
+    <form action="includes/adduser_submit" method="post">
+        <fieldset>
+            <p>
+            <label for="phpro_username">Username</label>
+            <input type="text" id="phpro_username" name="phpro_username" value="" maxlength="20" />
+            </p>
+            <p>
+            <label for="phpro_email">Email</label>
+            <input type="text" id="phpro_email" name="phpro_email" value="" maxlength="40" />
+            </p>
+            <p>
+            <label for="phpro_password">Password</label>
+            <input type="text" id="phpro_password" name="phpro_password" value="" maxlength="20" />
+            </p>
+            <p>
+            <input type="hidden" name="form_token" value="<?php echo $form_token; ?>" />
+            <input type="submit" value="&rarr; Login" />
+            </p>
+        </fieldset>
+    </form>
 </body>
 </html>
