52e4d4ac7fb902fdc681bfb612bb90c9bcabf4e3
Standard IP <rhroyston@gmail.com>
2015-08-16 19:55:35 +0000

    My changes

--- login.php
+++ login.php
@@ -15,33 +15,41 @@
         }
     ?>
     <body>
-        <?php include 'includes/header.php';?>
-        <section class="section">
+        <?php include 'includes/header-min.php';?>
+        <section>
             <div class="container">
-                <h2>Login</h2>
-                <form action="includes/login_submit" method="post">
-                    <fieldset>
-                        <span>
-                            <label for="phpro_username">Username</label>
-                            <input type="text" id="phpro_username" name="phpro_username" value="" maxlength="20" />
-                            <label for="phpro_password">Password</label>
-                            <input type="text" id="phpro_password" name="phpro_password" value="" maxlength="20" />
-                        </span>
-                        <p>
-                            <input type="submit" value="Login" />
-                        </p>
-                    </fieldset>
-                </form>
-                <aside>
-                    <?php
-                        if (isset($_SESSION["message"]))
-                        {
-                        $message = $_SESSION["message"];
-                        echo "<p class='red'>$message</p>";
-                        }
-                    ?>
-                    <p>This is a private computer system&#46;  Unauthorized access is scrictly prohibited&#46;</p>
-                </aside>
+                <div class="row">
+                    <div class="col-md-4">
+                    </div>
+                    
+                    <div class="col-md-4">
+                        <h2>Login</h2>
+                        <form action="includes/login_submit" method="post">
+                            <span>
+                                <label for="phpro_username">Username</label>
+                                <input type="text" id="phpro_username" name="phpro_username" value="" maxlength="20" />
+                                <label for="phpro_password">Password</label>
+                                <input type="text" id="phpro_password" name="phpro_password" value="" maxlength="20" />
+                            </span>
+                            <p>
+                                <input type="submit" value="Login" />
+                            </p>
+                        </form>
+                        <aside>
+                            <?php
+                                if (isset($_SESSION["message"]))
+                                {
+                                $message = $_SESSION["message"];
+                                echo "<p class='red'>$message</p>";
+                                }
+                            ?>
+                            <p>This is a private computer system&#46;  Unauthorized access is scrictly prohibited&#46;</p>
+                        </aside>
+                    </div>
+                    
+                    <div class="col-md-4">
+                    </div>
+                </div>
             </div>
         </section>
         <?php include 'includes/footer.php';?>
