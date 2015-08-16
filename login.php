c03f7135774c96955e043f0351ae5df27cc0b56f
Standard IP <rhroyston@gmail.com>
2015-08-16 21:25:53 +0000

    My changes

--- login.php
+++ login.php
@@ -63,7 +63,41 @@
                             </div>
                             
                            
-                            <div role="tabpanel" class="tab-pane" id="tab-register">Register Here
+                            <div role="tabpanel" class="tab-pane" id="tab-register">
+                                
+                                <h2>Add user</h2>
+                                <form action="includes/adduser_submit" method="post">
+                                    <fieldset>
+                                        <p>
+                                            <label for="phpro_username">Username</label>
+                                            <input type="text" id="phpro_username" name="phpro_username" value="" maxlength="20" />
+                                        </p>
+                                        <p>
+                                            <label for="phpro_email">Email</label>
+                                            <input type="text" id="phpro_email" name="phpro_email" value="" maxlength="40" />
+                                        </p>
+                                        <p>
+                                            <label for="phpro_password">Password</label>
+                                            <input type="text" id="phpro_password" name="phpro_password" value="" maxlength="20" />
+                                        </p>
+                                        <p>
+                                            <input type="hidden" name="form_token" value="<?php echo $form_token; ?>" />
+                                            <input type="submit" value="Add" />
+                                        </p>
+                                    </fieldset>
+                                </form>
+                                <aside>
+                                    <?php
+                                        if (isset($_SESSION["message"]))
+                                        {
+                                        $message = $_SESSION["message"];
+                                        echo "<p class='red'>$message</p>";
+                                        }
+                                    ?>
+                                </aside>                                
+                                
+                                
+                                
                             
                             </div>
                             
