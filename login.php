0b4a6933de235ba6400aa5cd92c4247d5d225793
Standard IP <rhroyston@gmail.com>
2015-08-16 20:17:28 +0000

    My changes

--- login.php
+++ login.php
@@ -16,58 +16,62 @@
     ?>
     <body>
         <?php include 'includes/header-min.php';?>
+        <br>
+        <br>
+        <br>
+        <br>
         <section>
             <div class="container">
                 <div class="row">
                     <div class="col-md-2">
                     </div>
-                    
                     <div class="col-md-8">
-
-                        
-<div>
-
-  <!-- Nav tabs -->
-  <ul class="nav nav-tabs" role="tablist">
-    <li role="presentation" class="active"><a href="#tab-login" aria-controls="home" role="tab" data-toggle="tab">Login</a></li>
-    <li role="presentation"><a href="#tab-register" aria-controls="profile" role="tab" data-toggle="tab">Register</a></li>
-  </ul>
-
-  <!-- Tab panes -->
-  <div class="tab-content">
-    <div role="tabpanel" class="tab-pane active" id="tab-login">Login Here</div>
-    <div role="tabpanel" class="tab-pane" id="tab-register">Register Here</div>
-  </div>
-
-</div>                        
-                        
-                        
-                        
-                        
-                        
-                        
-                        
-                        
-                        
-                        
-                        
-                        
-                        
-                        
-                        
-                        
-                        
-                        
-                        
-                        
+                        <div>
+                          <!-- Nav tabs -->
+                          <ul class="nav nav-tabs" role="tablist">
+                            <li role="presentation" class="active"><a href="#tab-login" aria-controls="home" role="tab" data-toggle="tab">Login</a></li>
+                            <li role="presentation"><a href="#tab-register" aria-controls="profile" role="tab" data-toggle="tab">Register</a></li>
+                          </ul>
+                          <!-- Tab panes -->
+                          <div class="tab-content">
+                            <div role="tabpanel" class="tab-pane active" id="tab-login">
+                                <h2>Login</h2>
+                                <form action="includes/login_submit" method="post">
+                                    <span>
+                                        <label for="phpro_username">Username</label>
+                                        <input type="text" id="phpro_username" name="phpro_username" value="" maxlength="20" />
+                                        <label for="phpro_password">Password</label>
+                                        <input type="text" id="phpro_password" name="phpro_password" value="" maxlength="20" />
+                                    </span>
+                                    <p>
+                                        <input type="submit" value="Login" />
+                                    </p>
+                                </form>
+                                <aside>
+                                    <?php
+                                        if (isset($_SESSION["message"]))
+                                        {
+                                        $message = $_SESSION["message"];
+                                        echo "<p class='red'>$message</p>";
+                                        }
+                                    ?>
+                                    <p>This is a private computer system&#46;  Unauthorized access is scrictly prohibited&#46;</p>
+                                </aside>                            
+                            </div>
+                            
+                            
+                            <div role="tabpanel" class="tab-pane" id="tab-register">Register Here
+                            
+                            </div>
+                            
+                          </div>
+                        </div>                        
                     </div>
-                    
-        
                     <div class="col-md-2">
                     </div>
                 </div>
             </div>
         </section>
-        <?php include 'includes/footer-min.php';?>
+    <h6 class="text-right"><small>&#169; Copyright 2011&#45;2016 &#124; Piano Pathways &#124; 9270 Siegen Lane #304 &#124; Baton Rouge&#44; LA 70810</small></h6>
     </body>
 </html>
