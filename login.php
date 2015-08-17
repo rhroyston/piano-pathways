b72b9ef46f8da4df1715093e2ae5e2593c934d2c
Standard IP <rhroyston@gmail.com>
2015-08-17 01:09:33 +0000

    My changes

--- login.php
+++ login.php
@@ -79,26 +79,29 @@
                                     </div>                                    
                                     <br>
 
-                                    <div class="form-group">
-                                        <label>Birthday</label>
-                                        <div class="input-group navbar-form">
-                                            <select class="form-control">
-                                                <?php include 'includes/day.php';?>
-                                            </select>
-                                            <select class="form-control">
-                                                <?php include 'includes/month.php';?>
-                                            </select>
-                                            <select class="form-control">
-                                                <?php include 'includes/year.php';?>
-                                            </select>
+                                    <div class="form-horizontal">
+                                        <div class="form-group col-lg-8">
+                                            <label>Birthday</label>
+                                            <div class="input-group navbar-form">
+                                                <select class="form-control">
+                                                    <?php include 'includes/day.php';?>
+                                                </select>
+                                                <select class="form-control">
+                                                    <?php include 'includes/month.php';?>
+                                                </select>
+                                                <select class="form-control">
+                                                    <?php include 'includes/year.php';?>
+                                                </select>
+                                            </div>
                                         </div>
+                                        <div class="form-group col-lg-1"></div>
+                                        <div class="form-group col-lg-4">
+                                        <label>Grade</label>
+                                        <select class="form-control">
+                                            <?php include 'includes/grade.php';?>
+                                        </select>
                                     </div>
                                     
-                                    <div class="form-group">
-                                    <label>Grade</label>
-                                    <select class="form-control">
-                                        <?php include 'includes/grade.php';?>
-                                    </select>
                                     <br>
                                     
                                     <div class="form-group">
@@ -107,20 +110,20 @@
                                     </div>
                                     
                                     <div class="form-horizontal">
-                                    <div class="form-group col-lg-6">
-                                        <label for="city">City</label>
-                                        <input type="text" class="form-control" id="city" placeholder="City">
-                                    </div>
-                                    <div class="form-group col-lg-1"></div>
-                                    <div class="form-group col-lg-4">
-                                        <label for="state">State</label>
-                                        <input type="text" class="form-control" id="state" placeholder="State">
-                                    </div>
-                                    <div class="form-group col-lg-1"></div>
-                                    <div class="form-group col-lg-2">
-                                        <label for="zip">Zip</label>
-                                        <input type="number" class="form-control" id="zip" placeholder="Zip">
-                                    </div>
+                                        <div class="form-group col-lg-6">
+                                            <label for="city">City</label>
+                                            <input type="text" class="form-control" id="city" placeholder="City">
+                                        </div>
+                                        <div class="form-group col-lg-1"></div>
+                                        <div class="form-group col-lg-4">
+                                            <label for="state">State</label>
+                                            <input type="text" class="form-control" id="state" placeholder="State">
+                                        </div>
+                                        <div class="form-group col-lg-1"></div>
+                                        <div class="form-group col-lg-2">
+                                            <label for="zip">Zip</label>
+                                            <input type="number" class="form-control" id="zip" placeholder="Zip">
+                                        </div>
                                     </div>
   
   
