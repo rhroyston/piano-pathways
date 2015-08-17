25d380bb15a2ae79ff5c85e72103f7457c59cab6
Standard IP <rhroyston@gmail.com>
2015-08-17 22:49:18 +0000

    My changes

--- login.php
+++ login.php
@@ -192,7 +192,7 @@
                                         //if (isset($_SESSION["message"])){
                                             $message = $_SESSION["message"];
                                             // if the target is the registration pane then display and then kill message
-                                            if(strpos($_SERVER['REQUEST_URI'],"/login#tab-register")===0){
+                                            //if(strpos($_SERVER['REQUEST_URI'],"/login#tab-register")===0){
                                                 
                                                 if (strpos($message,'Success') !== false) {
                                                     echo "<div class='alert alert-success'><a class='close' data-dismiss='alert'><i class='fa fa-times'></i></a><strong><i class='fa fa-check'></i> $message</strong></div>";
@@ -205,7 +205,7 @@
                                                 }
                                             $message = NULL;
                                             unset ($_SESSION["message"]);
-                                            }
+                                            //}
                                         //}
                                         
 
