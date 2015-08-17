6fee74fd9d4d45d8e037ff1def21c8a2c0f7eb9a
Standard IP <rhroyston@gmail.com>
2015-08-17 22:47:17 +0000

    My changes

--- login.php
+++ login.php
@@ -186,8 +186,28 @@
                                 </form>
                                 <aside>
                                     <?php
-                                    echo $message;
-                                    echo 'hi there';
+
+
+
+                                        //if (isset($_SESSION["message"])){
+                                            $message = $_SESSION["message"];
+                                            // if the target is the registration pane then display and then kill message
+                                            if(strpos($_SERVER['REQUEST_URI'],"/login#tab-register")===0){
+                                                
+                                                if (strpos($message,'Success') !== false) {
+                                                    echo "<div class='alert alert-success'><a class='close' data-dismiss='alert'><i class='fa fa-times'></i></a><strong><i class='fa fa-check'></i> $message</strong></div>";
+                                                }
+                                                if (strpos($message,'Info') !== false) {
+                                                    echo "<div class='alert alert-warning'><a class='close' data-dismiss='alert'><i class='fa fa-times'></i></a><strong><i class='fa fa-info-circle'></i> $message</strong></div>";
+                                                }                                            
+                                                if (strpos($message,'Error') !== false) {
+                                                    echo "<div class='alert alert-error'><a class='close' data-dismiss='alert'><i class='fa fa-times'></i></a><strong><i class='fa fa-exclamation-triangle'></i> $message</strong></div>";
+                                                }
+                                            $message = NULL;
+                                            unset ($_SESSION["message"]);
+                                            }
+                                        //}
+                                        
 
                                     ?>
                                 </aside>                                
