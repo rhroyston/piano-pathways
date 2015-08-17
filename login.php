8c9f57ebf8749e06d63848de2a4a28a50ff4d106
Standard IP <rhroyston@gmail.com>
2015-08-17 22:22:28 +0000

    My changes

--- login.php
+++ login.php
@@ -50,25 +50,23 @@
                                 </form>
                                 <aside>
                                     <?php
-                                        if (isset($_SESSION["message"]))
-                                        {
-                                            $message = $_SESSION["message"];
-                                            if (strpos($message,'Success') !== false) {
-                                                echo "<div class='alert alert-success'><a class='close' data-dismiss='alert'><i class='fa fa-times'></i></a><strong><i class='fa fa-check'></i> $message</strong></div>";
-                                            }
-                                            if (strpos($message,'Info') !== false) {
-                                                echo "<div class='alert alert-warning'><a class='close' data-dismiss='alert'><i class='fa fa-times'></i></a><strong><i class='fa fa-info-circle'></i> $message</strong></div>";
-                                            }                                            
-                                            if (strpos($message,'Error') !== false) {
-                                                echo "<div class='alert alert-error'><a class='close' data-dismiss='alert'><i class='fa fa-times'></i></a><strong><i class='fa fa-exclamation-triangle'></i> $message</strong></div>";
-                                            }                                            
-                                        }
-                                        // if the target is the registration pane then don't kill message
-                                        if(!strpos($_SERVER['REQUEST_URI'],"/login#tab-register")===0){
+                                        if (isset($_SESSION["message"])){
+                                            // if the target is the registration pane then don't display and don't kill message
+                                            if(!strpos($_SERVER['REQUEST_URI'],"/login#tab-register")===0){
+                                                $message = $_SESSION["message"];
+                                                if (strpos($message,'Success') !== false) {
+                                                    echo "<div class='alert alert-success'><a class='close' data-dismiss='alert'><i class='fa fa-times'></i></a><strong><i class='fa fa-check'></i> $message</strong></div>";
+                                                }
+                                                if (strpos($message,'Info') !== false) {
+                                                    echo "<div class='alert alert-warning'><a class='close' data-dismiss='alert'><i class='fa fa-times'></i></a><strong><i class='fa fa-info-circle'></i> $message</strong></div>";
+                                                }                                            
+                                                if (strpos($message,'Error') !== false) {
+                                                    echo "<div class='alert alert-error'><a class='close' data-dismiss='alert'><i class='fa fa-times'></i></a><strong><i class='fa fa-exclamation-triangle'></i> $message</strong></div>";
+                                                }
                                             $message = NULL;
                                             unset ($_SESSION["message"]);
-                                        }                                        
-                                        
+                                            }
+                                        }
                                     ?>
                                 </aside>                            
                             </div>
@@ -203,12 +201,23 @@
                                 </form>
                                 <aside>
                                     <?php
-                                        if (isset($_SESSION["message"]))
-                                        {
-                                            echo($message);                                           
+                                        if (isset($_SESSION["message"])){
+                                            // if the target is the registration pane then display and then kill message
+                                            if(strpos($_SERVER['REQUEST_URI'],"/login#tab-register")===0){
+                                                $message = $_SESSION["message"];
+                                                if (strpos($message,'Success') !== false) {
+                                                    echo "<div class='alert alert-success'><a class='close' data-dismiss='alert'><i class='fa fa-times'></i></a><strong><i class='fa fa-check'></i> $message</strong></div>";
+                                                }
+                                                if (strpos($message,'Info') !== false) {
+                                                    echo "<div class='alert alert-warning'><a class='close' data-dismiss='alert'><i class='fa fa-times'></i></a><strong><i class='fa fa-info-circle'></i> $message</strong></div>";
+                                                }                                            
+                                                if (strpos($message,'Error') !== false) {
+                                                    echo "<div class='alert alert-error'><a class='close' data-dismiss='alert'><i class='fa fa-times'></i></a><strong><i class='fa fa-exclamation-triangle'></i> $message</strong></div>";
+                                                }
+                                            $message = NULL;
+                                            unset ($_SESSION["message"]);
+                                            }
                                         }
-                                        $message = NULL;
-                                        unset ($_SESSION["message"]);
                                     ?>
                                 </aside>                                
                             </div>
