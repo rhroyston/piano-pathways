0a91f4ab57341f2d3e8c8557340946c7026b7772
Standard IP <rhroyston@gmail.com>
2015-08-18 04:40:23 +0000

    My changes

--- login.php
+++ login.php
@@ -56,7 +56,7 @@
                                             // if the target is login pane then display message and then kill the session variable 
                                             if($host == 'thepianopathway-rhroyston.rhcloud.com/login') {
                                                 if (strpos($message,'Success') !== false) {
-                                                    echo "<div class='alert alert-success alert-dismissible alert-auto'><a class='close' data-dismiss='alert'><i class='fa fa-times'></i></a><strong><i class='fa fa-check'></i> $message</strong></div>";
+                                                    echo $host . "<div class='alert alert-success alert-dismissible alert-auto'><a class='close' data-dismiss='alert'><i class='fa fa-times'></i></a><strong><i class='fa fa-check'></i> $message</strong></div>";
                                                 }
                                                 if (strpos($message,'Error') !== false) {
                                                     echo "<div class='alert alert-danger alert-dismissible alert-auto'><a class='close' data-dismiss='alert'><i class='fa fa-times'></i></a><strong><i class='fa fa-exclamation-triangle'></i> $message</strong></div>";
