8230681e58df15d51ca2961f02d774877727fdff
Standard IP <rhroyston@gmail.com>
2015-08-18 22:20:35 +0000

    My changes

--- login.php
+++ login.php
@@ -109,7 +109,6 @@
                                             <label for="phpro_firstname">Student First Name</label>
                                             <input type="text" class="form-control" id="phpro_firstname" name="phpro_firstname" placeholder="First Name" required>
                                         </div>
-                                        <div class="col-lg-1"></div>
                                         <div class="col-lg-6" id="lastname">
                                             <label for="phpro_lastname">Student Last Name</label>
                                             <input type="text" class="form-control" id="phpro_lastname" name="phpro_lastname" placeholder="Last Name" required>
@@ -121,15 +120,14 @@
                                             <label for="phpro_username">Telephone Number</label>
                                             <input type="tel" class="form-control" id="phpro_telephone" name="phpro_telephone" placeholder="Telephone Number" pattern="(?:\(\d{3}\)|\d{3})[- ]?\d{3}[- ]?\d{4}" required>
                                         </div>
-                                        <div class="col-lg-1"></div>
-                                        <div class="col-lg-8"></div>
-                                    </div>
-                                    
-                                    <div class="form-group form-horizontal">
                                         <div class="col-lg-8">
                                             <label for="phpro_street">Stree Address</label>
                                             <input type="text" class="form-control" id="phpro_street" name="phpro_street" value="" placeholder="Street Address" required>
                                         </div>
+                                    </div>
+                                    
+                                    <div class="form-group form-horizontal">
+
                                         <div class="col-lg-1"></div>
                                         <div class="col-lg-2" id="zip">
                                             <label for="zip">Zip</label>
