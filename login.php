f5ce9da771169a9afcac66222734dd80a0fc22a7
Standard IP <rhroyston@gmail.com>
2015-08-18 21:24:21 +0000

    My changes

--- login.php
+++ login.php
@@ -84,27 +84,27 @@
                                     unset ($_SESSION["message"]);
                                 ?>
                                 <form action="includes/adduser_submit" method="post" data-toggle="validator" id="registration">
-                                    <div class="input-group col-lg-8" id="username">
+                                    <div class="form-group col-lg-8" id="username">
                                         <label for="phpro_username">Student First Name</label>
                                         <input type="text" class="form-control" id="phpro_username" name="phpro_username" value="" placeholder="Enter A Username" maxlength="20" pattern="^[a-zA-Z][a-zA-Z0-9-_\.]{1,20}$" required autofocus/>
                                     </div>
                                     <br>
-                                    <div class="input-group col-lg-8" id="password">
+                                    <div class="form-group col-lg-8" id="password">
                                         <label for="phpro_username">Password</label>
                                         <input type="password" class="form-control" id="phpro_password" name="phpro_password" placeholder="Enter A Password" maxlength="20" required/>
                                     </div>
                                     <br>
-                                    <div class="input-group col-lg-8" id="password_confirm">
+                                    <div class="form-group col-lg-8" id="password_confirm">
                                         <label for="phpro_username">Confirm Password</label>
                                         <input type="password" class="form-control" id="phpro_password_confirm" name="phpro_password_confirm" placeholder="Confirm Password" maxlength="20" required/>
                                     </div>
                                     <br>
-                                    <div class="input-group col-lg-8" id="email">
+                                    <div class="form-group col-lg-8" id="email">
                                         <label for="phpro_username">Email</label>
                                         <input type="email" class="form-control" id="phpro_email" name="phpro_email" value="" placeholder="Enter Your Email Address" maxlength="40" required/>
                                     </div>                                    
                                     <br>
-                                    <div class="input-group col-lg-8" id="telephone">
+                                    <div class="form-group col-lg-8" id="telephone">
                                         <label for="phpro_username">Telephone Number</label>
                                         <input type="tel" class="form-control" id="phpro_telephone" name="phpro_telephone" placeholder="Telephone Number" pattern="(?:\(\d{3}\)|\d{3})[- ]?\d{3}[- ]?\d{4}" required>
                                     </div>
