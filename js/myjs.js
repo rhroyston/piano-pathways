c46c25d113621f61750dcd199af993ee90a267b5
Standard IP <rhroyston@gmail.com>
2015-08-18 06:30:37 +0000

    My changes

--- js/myjs.js
+++ js/myjs.js
@@ -20,7 +20,7 @@ $(document).ready(function() {
   $("#phpro_password_confirm").bind("change paste keyup", function() {
     if($('#phpro_password').val() != $('#phpro_password_confirm').val()) {
       //if phpro_password_confirm's has-error class is not set then toggle it on
-      if(!$("#phpro_password_confirm").hasClass("has-error")){
+      if($("#phpro_password_confirm").not.hasClass("has-error")){
         alert('does not have has-error class');
         $('#password_confirm').toggleClass("has-error");
       }
