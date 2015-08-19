5c1d4a725bdde44d0b506f1c4c690183d045a6f9
Standard IP <rhroyston@gmail.com>
2015-08-19 04:48:00 +0000

    My changes

--- login.php
+++ login.php
@@ -64,8 +64,8 @@
                                         </fieldset>
 
 
-                                        <div class="col-sm-2 text-center control">
-                                        <div class="input-group">
+                                        <div class="col-sm-2 control">
+                                        <div class="input-group text-center">
                                         <a class="btn btn-default btn-sm control pull-right" role="button" id="register-button"></i><i class="fa fa-pencil-square-o"></i> Register</a>
                                         </div>
                                         </div>
