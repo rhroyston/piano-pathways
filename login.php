0d838c0af8a0937725596cc9f29ec7d82af422a0
Standard IP <rhroyston@gmail.com>
2015-08-19 00:13:11 +0000

    My changes

--- login.php
+++ login.php
@@ -192,13 +192,15 @@
                                     <label for="lesson-choice-1">Lesson Preference &#40;Day and Time&#41;</label>
                                     <div class="input-group">
                                       <span class="input-group-addon lesson-pref" id="lesson-choice-1">1st Choice</span>
-                                      <input type="text" class="form-control" id="lesson-choice-1" name="lesson-choice-1">
-                                    
+                                      <input type="text" class="form-control lesson-pref-input" id="lesson-choice-1" name="lesson-choice-1">
+                                    <div class="input-group">
+                                    </div>
                                       <span class="input-group-addon lesson-pref" id="lesson-choice-2">2nd Choice</span>
-                                      <input type="text" class="form-control" id="lesson-choice-2" name="lesson-choice-2">
-                                    
+                                      <input type="text" class="form-control lesson-pref-input" id="lesson-choice-2" name="lesson-choice-2">
+                                    <div class="input-group">
+                                    </div>
                                       <span class="input-group-addon lesson-pref" id="lesson-choice-3">3rd Choice</span>
-                                      <input type="text" class="form-control" id="lesson-choice-3" name="lesson-choice-3">
+                                      <input type="text" class="form-control lesson-pref-input" id="lesson-choice-3" name="lesson-choice-3">
                                     </div>
                         
                                     <label>Choose a Payment Plan</label>
