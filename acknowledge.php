d70ab57926a94d3c3c84cdd91951cfb02c2b906c
Standard IP <rhroyston@gmail.com>
2015-08-21 18:39:57 +0000

    My changes

--- acknowledge.php
+++ acknowledge.php
@@ -5,17 +5,19 @@ if (isset($_POST['send'])) {
     
     
   
-     $to = 'david@example.com'; // Use your own email address
+     $to = 'rhroyston@gmail.com'; // Use your own email address
      $subject = 'Feedback from my site';  
      $message = 'Name: ' . $_POST['name'] . "\r\n\r\n";
      $message .= 'Email: ' . $_POST['email'] . "\r\n\r\n";
      $message .= 'Comments: ' . $_POST['comments'];
-     $headers = "From: webmaster@thepianopathway.com\r\n";
-$email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
-if ($email) {
-   $headers .= "\r\nReply-To: $email";
-}
+     
+     $headers = "From: ron@stndip.com\r\n";
+    $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+    if ($email) {
+       $headers .= "\r\nReply-To: $email";
+    }
      $headers .= 'Content-Type: text/plain; charset=utf-8';
+     
      $success = mail($to, $subject, $message, $headers);
 }
 ?>
