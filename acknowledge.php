5151c8bb5278f772abc1e64b05d6bcb5e3b530d5
Standard IP <rhroyston@gmail.com>
2015-08-21 19:22:18 +0000

    My changes

--- acknowledge.php
+++ acknowledge.php
@@ -1,38 +1,26 @@
 <?php
+include('Mail.php');
 if (isset($_POST['send'])) {
     
+$recipients = 'ron@stndip.com';
+
+$headers['From']    = 'richard@example.com';
+$headers['To']      = 'ron@stndip.com';
+$headers['Subject'] = 'Test message';
+
+$body = 'Test message';
+
+$params['sendmail_path'] = '/usr/lib/sendmail';
+
+// Create the mail object using the Mail::factory method
+$mail_object =& Mail::factory('sendmail', $params);
+
+$mail_object->send($recipients, $headers, $body);    
     
     
-    
-  
-     $to = 'rhroyston@gmail.com'; // Use your own email address
-     $subject = 'Feedback from my site';  
-     $message = 'Name: ' . $_POST['name'] . "\r\n\r\n";
-     $message .= 'Email: ' . $_POST['email'] . "\r\n\r\n";
-     $message .= 'Comments: ' . $_POST['comments'];
-     
-     $headers = "From: ron@stndip.com\r\n";
-    $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
-    if ($email) {
-       $headers .= "\r\nReply-To: $email";
-    }
-     $headers .= 'Content-Type: text/plain; charset=utf-8';
-     
-     $success = mail($to, $subject, $message, $headers);
+
 }
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-    
-<body>
-<?php if (isset($success) && $success) { ?>
-<h1>Thank You</h1>
-Your message has been sent.
-<?php } else { ?>
-<h1>Oops!</h1>
-Sorry, there was a problem sending your message.
-<?php } ?>
-</body>
 
-</html>
+
