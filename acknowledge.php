1ec89a8653051c7748dff6c041137b50305bc395
Standard IP <rhroyston@gmail.com>
2015-08-21 18:35:00 +0000

    My changes

--- acknowledge.php
+++ acknowledge.php
@@ -2,10 +2,7 @@
 if (isset($_POST['send'])) {
     
     
-$email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
-if ($email) {
-   $headers .= "\r\nReply-To: $email";
-}    
+    
     
   
      $to = 'david@example.com'; // Use your own email address
@@ -14,6 +11,10 @@ if ($email) {
      $message .= 'Email: ' . $_POST['email'] . "\r\n\r\n";
      $message .= 'Comments: ' . $_POST['comments'];
      $headers = "From: webmaster@thepianopathway.com\r\n";
+$email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+if ($email) {
+   $headers .= "\r\nReply-To: $email";
+}
      $headers .= 'Content-Type: text/plain; charset=utf-8';
      $success = mail($to, $subject, $message, $headers);
 }
