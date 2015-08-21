e684007695f56cc4a960437f07b0ec3b3c2fb6d8
Standard IP <rhroyston@gmail.com>
2015-08-21 19:30:43 +0000

    My changes

--- acknowledge.php
+++ acknowledge.php
@@ -1,26 +1,41 @@
 <?php
-include('Mail.php');
-if (isset($_POST['send'])) {
-    
-$recipients = 'ron@stndip.com';
-
-$headers['From']    = 'richard@example.com';
-$headers['To']      = 'ron@stndip.com';
-$headers['Subject'] = 'Test message';
-
-$body = 'Test message';
-
-$params['sendmail_path'] = '/usr/lib/sendmail';
-
-// Create the mail object using the Mail::factory method
-$mail_object =& Mail::factory('sendmail', $params);
-
-$mail_object->send($recipients, $headers, $body);    
-    
-    
-
-}
+// Include the Mail package
+   require "Mail.php";
+ 
+   // Identify the sender, recipient, mail subject, and body
+   $sender    = "rhroyston@gmail.com";
+   $recipient = "ron@stndip.com";
+   $subject   = "Test mail";
+   $body      = "Hello from Piano Pathways";
+ 
+   // Identify the mail server, username, password, and port
+   $server   = "ssl://smtp.gmail.com";
+   $username = "rhroyston@gmail.com";
+   $password = "racks0nRacks";
+   $port     = "465";
+ 
+   // Set up the mail headers
+   $headers = array(
+      "From"    => $sender,
+      "To"      => $recipient,
+      "Subject" => $subject
+   );
+ 
+   // Configure the mailer mechanism
+   $smtp = Mail::factory("smtp",
+      array(
+        "host"     => $server,
+        "username" => $username,
+        "password" => $password,
+        "auth"     => true,
+        "port"     => 465
+      )
+   );
+ 
+   // Send the message
+   $mail = $smtp->send($recipient, $headers, $body);
+ 
+   if (PEAR::isError($mail)) {
+      echo ($mail->getMessage());
+   }
 ?>
-
-
-
