0ad65982bb6e5d3354658585427ecc8fc3b52375
Standard IP <rhroyston@gmail.com>
2015-08-22 00:32:09 +0000

    My changes

---
+++ includes/alert.php
@@ -0,0 +1,13 @@
+<?php
+    if (isset($message)){
+        // display message and then kill the session variable 
+        if (strpos($message,'Success') !== false) {
+            echo "<div class='alert alert-success alert-dismissible alert-auto'><a class='close' data-dismiss='alert'><i class='fa fa-times'></i></a><strong><i class='fa fa-check'></i> $message</strong></div>";
+        }
+        if (strpos($message,'Error') !== false) {
+            echo "<div class='alert alert-danger alert-dismissible alert-auto'><a class='close' data-dismiss='alert'><i class='fa fa-times'></i></a><strong><i class='fa fa-exclamation-triangle'></i> $message</strong></div>";
+        }
+        $message = NULL;
+        $loginpane = NULL;
+    }
+?>
--- index.php
+++ index.php
@@ -22,6 +22,7 @@
     include 'includes/head.php';
   ?>
   <body>
+    <?php include 'includes/alert.php';?>
     <?php include 'includes/header.php';?>
     <?php include 'includes/banner.php';?>
     <div class="container-fluid">
