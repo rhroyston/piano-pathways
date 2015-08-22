712f1cbda7ec3850093ca706b1f6f815cd00fae5
Standard IP <rhroyston@gmail.com>
2015-08-22 20:46:48 +0000

    My changes

--- includes/header.php
+++ includes/header.php
@@ -22,11 +22,11 @@
             <img src="/images/logo.png">
         </a>
     </div>
-    <div class="row">
-      <h6 class="text-center adjustup">9270 Siegen Lane #304 Baton Rouge, LA 70810 &#40;225&#41; 767&#45;0030</h6>
-    </div>
-    <h5 class="text-center blue">BATON ROUGE&#39;S MUSIC CENTER FOR CHILDREN&#38; ADULTS</h5>
-    <h5 class="text-center blue">lessons &#124; recitals &#124; studio classes &#124; summer camps</h5>
+    <h5 class="text-center">BATON ROUGE&#39;S MUSIC CENTER FOR CHILDREN&#38; ADULTS</h5>
+    <h5 class="text-center">lessons &#124; recitals &#124; studio classes &#124; summer camps</h5>
+    
+    <h6 class="text-center adjustup blue"><a href="https://www.google.com/maps/place/9270+Siegen+Ln+%23304,+Baton+Rouge,+LA+70810/@30.3672887,-91.0751662,17z/data=!3m1!4b1!4m2!3m1!1s0x8626a53e88fc2d09:0xda2e043d4fe40d5d" target="blank"></a><i class="fa fa-map-marker"></i> 9270 Siegen Lane #304 Baton Rouge, LA 70810</a> &#40;225&#41; 767&#45;0030</h6>
+
     <br>
     <div class="text-center">
         <a class="btn btn-primary" href="tel:225-767-0030" role="button"><i class="fa fa-phone"></i> Telephone</a>
