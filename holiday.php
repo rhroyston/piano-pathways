cb84067dd9148230de327c92b3d47242cd5f2ed6
Standard IP <rhroyston@gmail.com>
2015-08-22 19:36:49 +0000

    My changes

--- holiday.php
+++ holiday.php
@@ -10,14 +10,22 @@
     <?php include 'includes/alert.php';?>
     <?php include 'includes/header.php';?>
     <?php include 'includes/banner.php';?>
-    <div class="container-fluid">
-      <div class="row adjustup">
-        <img src="/images/pianoman.png" class="img-responsive center-block">
-      </div>
-    </div>
-    <?php include 'includes/why.php';?>
-    <?php include 'includes/instructors.php';?>
-    <?php include 'includes/footer.php';?>
+    
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br><br>
+    merry christmas
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    
+        <?php include 'includes/footer.php';?>
     <?php include 'includes/resources.php';?>
   </body>
 </html>
