63c15c15c8255c5dc2c7676946d7449822ecccb7
Standard IP <rhroyston@gmail.com>
2015-08-23 15:13:47 +0000

    My changes

--- includes/menu.php
+++ includes/menu.php
@@ -11,15 +11,15 @@
     <div class="row">
         <div class="col-md-4">
             <i class="fa fa fa-users fa-5x fa-pull-left fa-border"></i>
-            <span class="xl">Who we are</span>
+            <span class="xl">Piano Pathways is the premier piano studio of Baton Rouge. With a team of professional piano teachers, our goal is to utilize the skills that we have mastered to be the very best in teaching you how to play the piano beautifully.</span>
+        </div>
+        <div class="col-md-4">
+            <i class="fa fa fa-music fa-5x fa-pull-left fa-border"></i>
+            <span class="xl">At Piano Pathways, we aim to provide all students an enjoyable lesson experience. We believe in fundamental teaching approaches while utilizing technology to enhance learning. We use a blend of acoustic and digital instruments and a mix of classical and popular music, all to give the student the most individualized instruction possible.</span>
         </div>
         <div class="col-md-4">
             <i class="fa fa fa-users fa-5x fa-pull-left fa-border"></i>
-            <span class="xl">What we teach</span>
-        </div>
-        <div class="col-md-4">
-            <i class="fa fa fa-users fa-5x fa-pull-left fa-border"></i>
-            <span class="xl">Why choose us</span>
+            <span class="xl">Piano Pathways is a professional music center that offers exceptional piano education for all ages and ability levels. Our goal is to give every student personalized piano instruction, with teaching approaches that make learning accessible.</span>
         </div>
     </div>
 </div>
