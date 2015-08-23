ed4fd248fcd3abf1f08358259d87f7ec0fb2f1a6
Standard IP <rhroyston@gmail.com>
2015-08-23 04:02:36 +0000

    My changes

--- test.php
+++ test.php
@@ -8,7 +8,7 @@
   ?>
   <body>
     <?php include 'includes/alert.php';?>
-    <?php //include 'includes/header.php';?>
+    <?php include 'includes/navbar.php';?>
     <?php //include 'includes/banner.php';?>
     <div class="container-fluid">
       <div class="row adjustup">
