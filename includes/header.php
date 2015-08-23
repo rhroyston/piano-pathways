cdb32a0a6422ac3f18112da51bae107afb758a44
Standard IP <rhroyston@gmail.com>
2015-08-23 13:48:55 +0000

    My changes

--- includes/header.php
+++ includes/header.php
@@ -9,11 +9,11 @@
             <?php
                 if(!isset($_SESSION['user_id']))
                 {
-                    echo '<span><a class="white" href="/login"><i class="fa fa-sign-in"></i> login</a></span>';
+                    echo '<span><a href="/login"><i class="fa fa-sign-in"></i> login</a></span>';
                 }
                 else
                 {
-                    echo '<span><a class="white" href="/logout"> <i class="fa fa-sign-in"></i> logout</a></span>';
+                    echo '<span><a href="/logout"> <i class="fa fa-sign-in"></i> logout</a></span>';
                 }
             ?>
     </h5>
@@ -22,16 +22,18 @@
             <img src="/images/logo-black-lg.png">
         </a>
     </div>
-    <h5 class="text-center adjustup">BATON ROUGE&#39;S MUSIC CENTER FOR CHILDREN&#38; ADULTS</h5>
+    <h6 class="text-center adjustup">BATON ROUGE&#39;S MUSIC CENTER FOR CHILDREN&#38; ADULTS</h6>
     <h5 class="text-center adjustup">lessons &#124; recitals &#124; studio classes &#124; summer camps</h5>
     
-    <h4 class="text-center blue"><a href="https://www.google.com/maps/place/9270+Siegen+Ln+%23304,+Baton+Rouge,+LA+70810/@30.3672887,-91.0751662,17z/data=!3m1!4b1!4m2!3m1!1s0x8626a53e88fc2d09:0xda2e043d4fe40d5d" target="blank"><i class="fa fa-map-marker"></i> 9270 Siegen Lane #304 Baton Rouge, LA 70810</a></h4>
-    <h4 class="text-center blue"><a href="tel:225-767-0030"><i class="fa fa-phone"></i> &#40;225&#41; 767&#45;0030</a></h4>
+    <h6 class="text-center"><a href="https://www.google.com/maps/place/9270+Siegen+Ln+%23304,+Baton+Rouge,+LA+70810/@30.3672887,-91.0751662,17z/data=!3m1!4b1!4m2!3m1!1s0x8626a53e88fc2d09:0xda2e043d4fe40d5d" target="blank"><i class="fa fa-map-marker"></i> 9270 Siegen Lane #304 Baton Rouge, LA 70810</a></h6>
+    <h4 class="text-center"><a href="tel:225-767-0030"><i class="fa fa-phone"></i> &#40;225&#41; 767&#45;0030</a></h4>
 
     <br>
     <div class="text-center">
-        <a class="btn btn-primary" href="tel:225-767-0030" role="button"><i class="fa fa-phone"></i> Telephone</a>
-        <a class="btn btn-primary" href="https://www.google.com/maps/place/9270+Siegen+Ln+%23304,+Baton+Rouge,+LA+70810/@30.3672887,-91.0751662,17z/data=!3m1!4b1!4m2!3m1!1s0x8626a53e88fc2d09:0xda2e043d4fe40d5d" target="blank" role="button"><i class="fa fa-map-marker"></i> Directions</a>
+        <a class="btn btn-primary" href="#" target="blank" role="button"><i class="fa fa-home"></i> Home</a>
+        <a class="btn btn-primary" href="#" target="blank" role="button"><i class="fa fa-music"></i> Studio</a>
+        <a class="btn btn-primary" href="#" target="blank" role="button"><i class="fa fa-users"></i> Instructors</a>
+        <a class="btn btn-primary" href="#" target="blank" role="button"><i class="fa fa-file-text-o"></i> Policy</a>
     </div>
     <br>
     <br>
