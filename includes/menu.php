2720cee6771ac9ee7399d2eb6a7ce5f56302a31b
Standard IP <rhroyston@gmail.com>
2015-08-23 15:02:57 +0000

    My changes

--- includes/menu.php
+++ includes/menu.php
@@ -9,16 +9,16 @@
 
 <div class="container">
     <div class="row">
-        <div class="col-md-8">
-            <i class="fa fa fa-users fa-3x fa-pull-left fa-border"></i>
+        <div class="col-md-4">
+            <i class="fa fa fa-users fa-5x fa-pull-left fa-border"></i>
             <span class="xl">Who we are</span>
         </div>
-        <div class="col-md-8">
-            <i class="fa fa fa-users fa-3x fa-pull-left fa-border"></i>
+        <div class="col-md-4">
+            <i class="fa fa fa-users fa-5x fa-pull-left fa-border"></i>
             <span class="xl">What we teach</span>
         </div>
         <div class="col-md-4">
-            <i class="fa fa fa-users fa-3x fa-pull-left fa-border"></i>
+            <i class="fa fa fa-users fa-5x fa-pull-left fa-border"></i>
             <span class="xl">Why choose us</span>
         </div>
     </div>
