bcc6323dfe6def6d05782b0e14e835569fc05b2b
Standard IP <rhroyston@gmail.com>
2015-08-23 13:58:46 +0000

    My changes

--- includes/header.php
+++ includes/header.php
@@ -23,11 +23,9 @@
         </a>
     </div>
     <h6 class="text-center adjustup">BATON ROUGE&#39;S MUSIC CENTER FOR CHILDREN&#38; ADULTS</h6>
-    <h5 class="text-center adjustup">lessons &#124; recitals &#124; studio classes &#124; summer camps</h5>
-    
     <h6 class="text-center"><a href="https://www.google.com/maps/place/9270+Siegen+Ln+%23304,+Baton+Rouge,+LA+70810/@30.3672887,-91.0751662,17z/data=!3m1!4b1!4m2!3m1!1s0x8626a53e88fc2d09:0xda2e043d4fe40d5d" target="blank"><i class="fa fa-map-marker"></i> 9270 Siegen Lane #304 Baton Rouge, LA 70810</a></h6>
+    <br>
     <h4 class="text-center"><a href="tel:225-767-0030"><i class="fa fa-phone"></i> &#40;225&#41; 767&#45;0030</a></h4>
-
     <br>
     <div class="text-center">
         <a class="btn btn-primary" href="#" target="blank" role="button"><i class="fa fa-home"></i> Home</a>
