e94bc105b289e97d77da4edd9de41265f267e0d4
Standard IP <rhroyston@gmail.com>
2015-08-23 14:10:38 +0000

    My changes

--- index.1.php
+++ index.1.php
@@ -9,7 +9,7 @@
   <body>
     <?php include 'includes/alert.php';?>
     <?php include 'includes/header.php';?>
-
+    <?php include 'includes/banner.php';?>
     <div class="container-fluid">
       <div class="row adjustup">
         <img src="/images/pianoman.png" class="img-responsive center-block">
