4ea6b41bea558723ea7ac932b3ae178c20eb0152
Standard IP <rhroyston@gmail.com>
2015-08-23 21:59:05 +0000

    My changes

--- index.php
+++ index.php
@@ -18,11 +18,11 @@
           <?php
               if(!isset($_SESSION['user_id']))
               {
-                echo '<span><a href="/login" class="black"><i class="fa fa-sign-in"></i> login</a></span>';
+                echo '<span><a href="/login" class="btn btn-default btn-xs"><i class="fa fa-sign-in"></i> login</a></span>';
               }
               else
               {
-                echo '<span><a href="/logout" class="black"> <i class="fa fa-sign-in"></i> logout</a></span>';
+                echo '<span><a href="/logout" class="btn btn-default btn-xs"> <i class="fa fa-sign-in"></i> logout</a></span>';
               }
             ?>
         &nbsp;
