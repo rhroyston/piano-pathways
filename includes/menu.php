1a8cc7cc0d7fbffffe7610b0174cc19e701ca72f
Standard IP <rhroyston@gmail.com>
2015-08-23 15:19:35 +0000

    My changes

--- includes/menu.php
+++ includes/menu.php
@@ -11,11 +11,11 @@
     <div class="row">
         <div class="col-md-4">
             <i class="fa fa fa-users fa-5x fa-pull-left fa-border"></i>
-            <span class="xl">Piano Pathways is the premier piano studio of Baton Rouge. With a team of professional piano teachers, our goal is to utilize the skills that we have mastered to be the very best in teaching you how to play the piano beautifully.</span>
+            <span class="xl">With a team of professional piano teachers, our goal is to utilize the skills that we have mastered to be the very best in teaching you how to play the piano beautifully.</span>
         </div>
         <div class="col-md-4">
             <i class="fa fa fa-music fa-5x fa-pull-left fa-border"></i>
-            <span class="xl">At Piano Pathways, we aim to provide all students an enjoyable lesson experience. We believe in fundamental teaching approaches while utilizing technology to enhance learning. We use a blend of acoustic and digital instruments and a mix of classical and popular music, all to give the student the most individualized instruction possible.</span>
+            <span class="xl">We aim to provide all students an enjoyable lesson experience. We believe in fundamental teaching approaches while utilizing technology to enhance learning. We use a blend of acoustic and digital instruments and a mix of classical and popular music, all to give the student the most individualized instruction possible.</span>
         </div>
         <div class="col-md-4">
             <i class="fa fa fa-users fa-5x fa-pull-left fa-border"></i>
