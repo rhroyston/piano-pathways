6e203afd9bdeb2534e9d7ed90a3e88d8c0dce275
Standard IP <rhroyston@gmail.com>
2015-08-23 04:39:17 +0000

    My changes

--- includes/navbar.php
+++ includes/navbar.php
@@ -8,7 +8,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="#">Brand</a>
+      <a class="navbar-brand" href="#"><img alt="Brand" src="images/piano-icon.png"></a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
