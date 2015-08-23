8ae74402d513ed6630c8e55242366252735b6fb4
Standard IP <rhroyston@gmail.com>
2015-08-23 21:42:59 +0000

    My changes

--- includes/footer.php
+++ includes/footer.php
@@ -48,12 +48,15 @@
       </div>      
     </div>
   </div>
-
+  <br>
+  <br>
+  <br>
+  <br>
       <div class="col-md-1">
       </div>
     <div class="row">
       <div class="col-md-10">
-        <h6 class="text-center"><small>&#169; Copyright 2011&#45;2016 &#124; Piano Pathways &#124; 9270 Siegen Lane #304 &#124; Baton Rouge&#44; LA 70810</small></h6>
+        <h6 class="text-center black"><small>&#169; Copyright 2011&#45;2016 &#124; Piano Pathways &#124; 9270 Siegen Lane #304 &#124; Baton Rouge&#44; LA 70810</small></h6>
       </div>
     </div>
     <div class="col-md-1">
