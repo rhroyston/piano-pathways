d3164899076936bbfb6d8c5727bb06e0ea378523
Standard IP <rhroyston@gmail.com>
2015-08-23 15:22:16 +0000

    My changes

--- includes/menu.php
+++ includes/menu.php
@@ -18,7 +18,7 @@
             <span class="xl">We aim to provide all students an enjoyable lesson experience. We believe in fundamental teaching approaches while utilizing technology to enhance learning. We use a blend of acoustic and digital instruments and a mix of classical and popular music, all to give the student the most individualized instruction possible.</span>
         </div>
         <div class="col-md-4">
-            <i class="fa fa fa-users fa-5x fa-pull-left fa-border"></i>
+            <i class="fa fa fa-quote-left fa-5x fa-pull-left fa-border"></i>
             <span class="xl">Piano Pathways is a professional music center that offers exceptional piano education for all ages and ability levels. Our goal is to give every student personalized piano instruction, with teaching approaches that make learning accessible.</span>
         </div>
     </div>
