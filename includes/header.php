4dee2576df82846f13cf3e163dbfee733df39ec3
Standard IP <rhroyston@gmail.com>
2015-08-23 13:13:41 +0000

    My changes

--- includes/header.php
+++ includes/header.php
@@ -22,11 +22,11 @@
             <img src="/images/logo.png">
         </a>
     </div>
-    <h5 class="text-center">BATON ROUGE&#39;S MUSIC CENTER FOR CHILDREN&#38; ADULTS</h5>
-    <h5 class="text-center">lessons &#124; recitals &#124; studio classes &#124; summer camps</h5>
+    <h5 class="text-center adjustup">BATON ROUGE&#39;S MUSIC CENTER FOR CHILDREN&#38; ADULTS</h5>
+    <h5 class="text-center adjustup">lessons &#124; recitals &#124; studio classes &#124; summer camps</h5>
     
-    <h4 class="text-center adjustup blue"><a href="https://www.google.com/maps/place/9270+Siegen+Ln+%23304,+Baton+Rouge,+LA+70810/@30.3672887,-91.0751662,17z/data=!3m1!4b1!4m2!3m1!1s0x8626a53e88fc2d09:0xda2e043d4fe40d5d" target="blank"><i class="fa fa-map-marker"></i> 9270 Siegen Lane #304 Baton Rouge, LA 70810</a></h4>
-    <h4 class="text-center adjustup blue"><a href="tel:225-767-0030"><i class="fa fa-phone"></i> &#40;225&#41; 767&#45;0030</a></h4>
+    <h4 class="text-center blue"><a href="https://www.google.com/maps/place/9270+Siegen+Ln+%23304,+Baton+Rouge,+LA+70810/@30.3672887,-91.0751662,17z/data=!3m1!4b1!4m2!3m1!1s0x8626a53e88fc2d09:0xda2e043d4fe40d5d" target="blank"><i class="fa fa-map-marker"></i> 9270 Siegen Lane #304 Baton Rouge, LA 70810</a></h4>
+    <h4 class="text-center blue"><a href="tel:225-767-0030"><i class="fa fa-phone"></i> &#40;225&#41; 767&#45;0030</a></h4>
 
     <br>
     <div class="text-center">
