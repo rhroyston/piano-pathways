79305ce504b3625bdf77a17728f52e3c95912b9c
Standard IP <rhroyston@gmail.com>
2015-08-23 18:27:57 +0000

    My changes

--- includes/menu.php
+++ includes/menu.php
@@ -12,7 +12,7 @@
         <div class="col-sm-1"></div>
         <div class="col-sm-10">
             <i class="fa fa-quote-left fa-2x fa-pull-left"></i>
-            <span class="xl">Piano Pathways is a professional music center that offers exceptional piano education for all ages and ability levels. Our goal is to give every student personalized piano instruction, with teaching approaches that make learning accessible.
+            <span class="xl lobster">Piano Pathways is a professional music center that offers exceptional piano education for all ages and ability levels. Our goal is to give every student personalized piano instruction, with teaching approaches that make learning accessible.
             </span><i class="fa fa-quote-right fa-2x"></i>
         </div>
         <div class="col-sm-1"></div>
