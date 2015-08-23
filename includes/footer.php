93eb00ade0704ea9f7a9a2088a3f2a415312519d
Standard IP <rhroyston@gmail.com>
2015-08-23 21:02:22 +0000

    My changes

--- includes/footer.php
+++ includes/footer.php
@@ -37,17 +37,24 @@
     <br>
     <br>
     <div class="row">
-        <div class="col-md-4">
-        </div>
-        <div class="col-md-4">
+        <div class="col-md-6">
         </div>
         <div class="col-md-4">
           <h1 class="text-right blue">Piano Pathways</h1>
           <h6 class="text-right blue">MUSIC CENTER FOR CHILDREN&#38; ADULTS</h6>
           <h5 class="text-right blue"><b>lessons &#124; recitals &#124; studio classes &#124; summer camps</b></h5>
         </div>
+        <div class="col-md-2">
+
+        </div>
+    </div>
+    <div class="row">
+      <div class="col-md-10">
+        <h6 class="text-right"><small>&#169; Copyright 2011&#45;2016 &#124; Piano Pathways &#124; 9270 Siegen Lane #304 &#124; Baton Rouge&#44; LA 70810</small></h6>
+      </div>
     </div>
     <br>
-    <h6 class="text-right"><small>&#169; Copyright 2011&#45;2016 &#124; Piano Pathways &#124; 9270 Siegen Lane #304 &#124; Baton Rouge&#44; LA 70810</small></h6>
+    
+  <br>
   </div>
 </div>
