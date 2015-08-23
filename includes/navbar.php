7e2e23472733c877a8055290b219d3772a172216
Standard IP <rhroyston@gmail.com>
2015-08-23 04:48:39 +0000

    My changes

--- includes/navbar.php
+++ includes/navbar.php
@@ -9,7 +9,7 @@
         <span class="icon-bar"></span>
       </button>
       <a class="navbar-brand" href="#">
-          <img alt="Brand" src="images/piano-icon.png" height="32px">
+          <img alt="Brand" src="images/piano-icon.png" height="56px">
       </a>
     </div>
 
