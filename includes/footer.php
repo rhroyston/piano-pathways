9316d795f0e85c6a536d034594dd81462a4c9202
Standard IP <rhroyston@gmail.com>
2015-08-23 21:27:42 +0000

    My changes

--- includes/footer.php
+++ includes/footer.php
@@ -15,19 +15,23 @@
   <br>
   <br>
   <br>
+  <br>
+  <br>
+  <br>
+  <br>
   <div class="container">
     <div class="row">
       <div class="col-md-2">
       </div>
         <div class="col-md-8">
-            <h3 class="white">Piano Pathways Resources</h3>
-            <a href="#faqModal" data-toggle="modal"><i class="fa fa-question-circle"></i> FAQ&#39;s <i class="fa fa-angle-double-right"></i></a><br>
-            <a href="policy"><i class="fa fa-info-circle"></i> Studio Policy</a><br>
-            <a href="#schedulingModal" data-toggle="modal">Scheduling <i class="fa fa-angle-double-right"></i></a><br>
-            <a href="#tuitionModal" data-toggle="modal">Tuition <i class="fa fa-angle-double-right"></i></a><br>
-            <a href="#wheretuitiongoesModal" data-toggle="modal">Where Does Tuition Go&#63; <i class="fa fa-angle-double-right"></i></a><br>
-            <a href="http://www.musiclearningcommunity.com" target="blank">Make A Payment <i class="fa fa-external-link"></i></a><br>
-            <a href="http://www.encoremusiclessons.com/images/infographic/infographic-piano-lessons-are-good-for-you-and-your-brain.png" target="blank">Benefits of Piano Lessons <i class="fa fa-external-link"></i></a><br>
+            <h3 class="black">Piano Pathways Resources</h3>
+            <a class="black" href="#faqModal" data-toggle="modal"><i class="fa fa-question-circle"></i> FAQ&#39;s <i class="fa fa-angle-double-right"></i></a><br>
+            <a class="black" href="policy"><i class="fa fa-info-circle"></i> Studio Policy</a><br>
+            <a class="black" href="#schedulingModal" data-toggle="modal">Scheduling <i class="fa fa-angle-double-right"></i></a><br>
+            <a class="black" href="#tuitionModal" data-toggle="modal">Tuition <i class="fa fa-angle-double-right"></i></a><br>
+            <a class="black" href="#wheretuitiongoesModal" data-toggle="modal">Where Does Tuition Go&#63; <i class="fa fa-angle-double-right"></i></a><br>
+            <a class="black" href="http://www.musiclearningcommunity.com" target="blank">Make A Payment <i class="fa fa-external-link"></i></a><br>
+            <a class="black" href="http://www.encoremusiclessons.com/images/infographic/infographic-piano-lessons-are-good-for-you-and-your-brain.png" target="blank">Benefits of Piano Lessons <i class="fa fa-external-link"></i></a><br>
         </div>
       <div class="col-md-2">
       </div>
@@ -37,12 +41,11 @@
       </div>
     <div class="row">
       <div class="col-md-10">
-        <h6><small>&#169; Copyright 2011&#45;2016 &#124; Piano Pathways &#124; 9270 Siegen Lane #304 &#124; Baton Rouge&#44; LA 70810</small></h6>
+        <h6 class="text-center"><small>&#169; Copyright 2011&#45;2016 &#124; Piano Pathways &#124; 9270 Siegen Lane #304 &#124; Baton Rouge&#44; LA 70810</small></h6>
       </div>
     </div>
     <div class="col-md-1">
     </div>
-    <br>
-  <br>
+
   </div>
 </div>
