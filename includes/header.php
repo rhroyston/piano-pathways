debc5cb224439b002db36c7564c8cd3ef3823fc8
Standard IP <rhroyston@gmail.com>
2015-08-23 18:09:04 +0000

    My changes

--- includes/header.php
+++ includes/header.php
@@ -10,10 +10,10 @@
     <h4 class="text-center"><a class="black" href="tel:225-767-0030"><i class="fa fa-phone"></i> &#40;225&#41; 767&#45;0030</a></h4>
     <br>
     <div class="text-center">
-        <a class="btn" href="#" target="blank" role="button"><i class="fa fa-home"></i> Home</a>
-        <a class="btn" href="#" target="blank" role="button"><i class="fa fa-music"></i> Studio</a>
-        <a class="btn" href="#" target="blank" role="button"><i class="fa fa-users"></i> Instructors</a>
-        <a class="btn" href="#" target="blank" role="button"><i class="fa fa-file-text-o"></i> Policy</a>
+        <a class="btn btn-default" href="#" target="blank" role="button"><i class="fa fa-home"></i> Home</a>
+        <a class="btn btn-default" href="#" target="blank" role="button"><i class="fa fa-music"></i> Studio</a>
+        <a class="btn btn-default" href="#" target="blank" role="button"><i class="fa fa-users"></i> Instructors</a>
+        <a class="btn btn-default" href="#" target="blank" role="button"><i class="fa fa-file-text-o"></i> Policy</a>
     </div>
     <br>
     <br>
