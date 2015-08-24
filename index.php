a79d4c756e4527dd85aeded11c9c874fdf1562db
Standard IP <rhroyston@gmail.com>
2015-08-24 18:26:29 +0000

    My changes

--- index.php
+++ index.php
@@ -17,13 +17,13 @@
     <br>
     <div class="row text-center">
         <h1>
-        <div class="col-md-3 col-md-offset-1">
+        <div class="col-md-2 col-md-offset-3">
             <a class="black textshadow" href="about">Who We Are</a>
         </div>    
-        <div class="col-md-4">
+        <div class="col-md-2">
             <a class="black textshadow" href="instructors">Our Instructors</a>
         </div>    
-        <div class="col-md-3">
+        <div class="col-md-2">
             <a class="black textshadow" href="studio">View The Studio</a>
         </div>    
         </h1>
