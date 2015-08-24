d6fe8b296b83b7015318a66b9c07375f5f6bda94
Standard IP <rhroyston@gmail.com>
2015-08-24 01:30:25 +0000

    My changes

--- includes/header.php
+++ includes/header.php
@@ -17,19 +17,19 @@
     <br>
     <br>
     <br>
-    <div class="row">
+    <div class="row text-center">
         <h3>
         <div class="col-md-3">
-            <a class="black textshadow" href="#" target="blank" role="button"><i class="fa fa-users fa-5x"></i><br>Meet Our Instructors</a>
+            <a class="black textshadow" href="#" target="blank" role="button"><i class="fa fa-users fa-3x"></i><br>Meet Our Instructors</a>
         </div>    
         <div class="col-md-3">
-            <a class="black textshadow" href="#" target="blank" role="button"><i class="fa fa-music fa-5x"></i><br>View The Studio</a>
+            <a class="black textshadow" href="#" target="blank" role="button"><i class="fa fa-music fa-3x"></i><br>View The Studio</a>
         </div>    
         <div class="col-md-3">
-            <a class="black textshadow" href="#" target="blank" role="button"><i class="fa fa-street-view fa-5x"></i><br>Visit Us</a>
+            <a class="black textshadow" href="#" target="blank" role="button"><i class="fa fa-street-view fa-3x"></i><br>Visit Us</a>
         </div>    
         <div class="col-md-3">
-            <a class="black textshadow" href="#" target="blank" role="button"><i class="fa fa-phone fa-5x"></i><br>Give Us A Call</a>
+            <a class="black textshadow" href="#" target="blank" role="button"><i class="fa fa-phone fa-3x"></i><br>Give Us A Call</a>
         </div>    
         </h3>
     </div>
