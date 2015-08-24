a584db8565656493d4e4c14fb0722ade2da8b275
Standard IP <rhroyston@gmail.com>
2015-08-24 02:32:17 +0000

    My changes

--- instructors.php
+++ instructors.php
@@ -1,3 +1,16 @@
+<?php include 'includes/session.php';?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <?php 
+    $title = 'Login';
+    include 'includes/head.php';
+  ?>
+  <body>
+    <?php include 'includes/socialnav.php';?>
+    <?php include 'includes/alert.php';?>
+    <?php include 'includes/header.php';?>
+
 <div class="container">
   <br>
   <br>
@@ -174,3 +187,8 @@
 
   </div>
 </div>
+
+    <?php include 'includes/footer.php';?>
+    <?php include 'includes/resources.php';?>
+  </body>
+</html>
