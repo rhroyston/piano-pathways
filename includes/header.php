8f688f192f39d18315b1a3ce3bce33f89e96e41d
Standard IP <rhroyston@gmail.com>
2015-08-24 00:10:24 +0000

    My changes

--- includes/header.php
+++ includes/header.php
@@ -20,12 +20,14 @@
     <br>
     <br>
     <br>
+    <h3>
     <div class="text-center">
         <a class="black textshadow" href="#" target="blank" role="button"><i class="fa fa-users"></i> Instructors</a>
         <a class="black textshadow" href="#" target="blank" role="button"><i class="fa fa-music"></i> Studio</a>
         <a class="black textshadow" href="#" target="blank" role="button"><i class="fa fa-street-view"></i> Contact</a>
         <a class="black textshadow" href="#" target="blank" role="button"><i class="fa fa-phone"></i> Call</a>
     </div>
+    </h3>
     <br>
     <br>
     <br>
