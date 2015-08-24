327052d1878fd2037e638ad59df433752946914d
Standard IP <rhroyston@gmail.com>
2015-08-24 00:01:07 +0000

    My changes

--- index.php
+++ index.php
@@ -10,19 +10,19 @@
     
     <h4 class="text-right">
         <!-- Button trigger modal -->
-        <a class="black" href="https://twitter.com/PianoPathwaysBR"><i class="fa fa-twitter"></i></a>&nbsp;&nbsp;
-        <a class="black" href="https://www.facebook.com/PianoPathwaysStudio"><i class="fa fa-facebook-square"></i></a>&nbsp;&nbsp;
-        <a class="black" href="https://plus.google.com/114075402524593080864/posts"><i class="fa fa-google-plus"></i></a>&nbsp;&nbsp;
-        <a class="black" href="https://www.linkedin.com/pub/rebecca-bellelo/39/4a6/73"><i class="fa fa-linkedin"></i></a>&nbsp;&nbsp;
-        <a class="black" href="https://www.youtube.com/user/rbellelo"><i class="fa fa-youtube-play"></i></a>&nbsp;&nbsp;
+        <a class="black textshadow" href="https://twitter.com/PianoPathwaysBR"><i class="fa fa-twitter"></i></a>&nbsp;&nbsp;
+        <a class="black textshadow" href="https://www.facebook.com/PianoPathwaysStudio"><i class="fa fa-facebook-square"></i></a>&nbsp;&nbsp;
+        <a class="black textshadow" href="https://plus.google.com/114075402524593080864/posts"><i class="fa fa-google-plus"></i></a>&nbsp;&nbsp;
+        <a class="black textshadow" href="https://www.linkedin.com/pub/rebecca-bellelo/39/4a6/73"><i class="fa fa-linkedin"></i></a>&nbsp;&nbsp;
+        <a class="black textshadow" href="https://www.youtube.com/user/rbellelo"><i class="fa fa-youtube-play"></i></a>&nbsp;&nbsp;
           <?php
               if(!isset($_SESSION['user_id']))
               {
-                echo '<span><a href="/login" class="btn btn-default btn-xs"><i class="fa fa-sign-in"></i> login</a></span>';
+                echo '<span><a href="/login" class="textshadow"><i class="fa fa-sign-in"></i> login</a></span>';
               }
               else
               {
-                echo '<span><a href="/logout" class="btn btn-default btn-xs"> <i class="fa fa-sign-in"></i> logout</a></span>';
+                echo '<span><a href="/logout" class="textshadow"> <i class="fa fa-sign-in"></i> logout</a></span>';
               }
             ?>
         &nbsp;
