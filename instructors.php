12cae7b0c010100be9f9b6e680630e3e775df788
Standard IP <rhroyston@gmail.com>
2015-08-24 03:08:09 +0000

    My changes

--- instructors.php
+++ instructors.php
@@ -22,7 +22,14 @@
           <img src="/images/rebecca.png" class="instructors"><h3 class="instructoradjust">Rebecca Bellelo</h3><h6>Studio Founder&#44; Owner&#44; and Piano Faculty</h6>A National Certified Teacher of Piano&#44; Rebecca C&#46; Bellelo holds a Ph&#46;D&#46; in music from LSU&#44; and a Master of Music degree in Piano Pedagogy from LSU&#46; She earned a Bachelor of Music degree in piano performance from Delta State University&#46; As a private piano teacher for the past ten years to pre&#45;college level students&#44; her students have been honored for their performances by winning numerous awards in local and state festivals&#44; such as the district and state MTNA competition&#44; Bach & Sonatina festival competition at LSU&#44; and BRMTA Tournament&#46;
           <!-- Trigger the modal with a button -->
           <a data-toggle="collapse" href="#collapse-rebecca" aria-expanded="false" aria-controls="collapse-rebecca"> more <i class="fa fa-angle-double-right"></i></a>
-
+          <div class="collapse" id="collapse-rebecca">
+            <div class="well">
+                  <p>Rebecca founded Piano Pathways in 2011 with the goal to offer the Baton Rouge community a piano&#45;centered music studio&#44; where any student with any desire to play piano could be accepted and learn to play the instrument&#46; Rebecca serves as a teacher trainer/mentor to new Recreational Music Making (RMM) teachers awarded the National Piano Foundation RMM Teaching Fellowship&#46; She has served as an active adjudicator&#44; clinician&#44; and presenter at several state and national conferences&#44; including Music Teachers National Association Conferences&#44; National Conference on Keyboard Pedagogy&#44; National Association for Music Education conference&#44; Louisiana Music Teachers Association state conferences&#44; and Louisiana Music Educators Association conferences&#46; Rebecca is also an adjunct lecturer at Southeastern Louisiana University&#44; where she teaches class piano and piano pedagogy&#46; Rebecca remains current in the technological advancements in the pedagogy field and uses these tools to provide an invaluable opportunity for students in the classroom&#46;
+                  </p>
+                  <p>Rebecca&#39;s passionate desire to teach and enrich students through music is apparent in her teaching philosophy that every person is musical&#46; Every lesson consists of opportunities to expand the student&#39;s understandings of music&#44; integrating in every lesson performance&#44; reading&#44; technique&#44; and musicianship&#46; She incorporates a wide variety of activities that foster a healthy learning environment while allowing the student to grow as a well&#45;rounded individual&#46;
+                  </p>
+            </div>
+          </div> 
 
          
           
@@ -68,14 +75,7 @@
 </div>
 
 <!-- Collapse -->
-<div class="collapse" id="collapse-rebecca">
-  <div class="well">
-        <p>Rebecca founded Piano Pathways in 2011 with the goal to offer the Baton Rouge community a piano&#45;centered music studio&#44; where any student with any desire to play piano could be accepted and learn to play the instrument&#46; Rebecca serves as a teacher trainer/mentor to new Recreational Music Making (RMM) teachers awarded the National Piano Foundation RMM Teaching Fellowship&#46; She has served as an active adjudicator&#44; clinician&#44; and presenter at several state and national conferences&#44; including Music Teachers National Association Conferences&#44; National Conference on Keyboard Pedagogy&#44; National Association for Music Education conference&#44; Louisiana Music Teachers Association state conferences&#44; and Louisiana Music Educators Association conferences&#46; Rebecca is also an adjunct lecturer at Southeastern Louisiana University&#44; where she teaches class piano and piano pedagogy&#46; Rebecca remains current in the technological advancements in the pedagogy field and uses these tools to provide an invaluable opportunity for students in the classroom&#46;
-        </p>
-        <p>Rebecca&#39;s passionate desire to teach and enrich students through music is apparent in her teaching philosophy that every person is musical&#46; Every lesson consists of opportunities to expand the student&#39;s understandings of music&#44; integrating in every lesson performance&#44; reading&#44; technique&#44; and musicianship&#46; She incorporates a wide variety of activities that foster a healthy learning environment while allowing the student to grow as a well&#45;rounded individual&#46;
-        </p>
-  </div>
-</div> 
+
 
 
 
