6635c7a0d1ecd8db4548a558dedd0064ed306db5
Standard IP <rhroyston@gmail.com>
2015-08-25 19:27:58 +0000

    My changes

--- includes/header-min.php
+++ includes/header-min.php
@@ -1,14 +1,14 @@
 <div class="container-fluid header">
     <div class="logo-wrap">
         <a href="/" class="logo">
-            <img src="/images/logo.png">
+            <img src="/images/logo-black.png">
         </a>
     </div>
     <div class="row">
-      <h6 class="text-center adjustup">9270 Siegen Lane #304 Baton Rouge, LA 70810 &#40;225&#41; 767&#45;0030</h6>
+      <h6 class="text-center black adjustup">9270 Siegen Lane #304 Baton Rouge, LA 70810 &#40;225&#41; 767&#45;0030</h6>
     </div>
-    <h5 class="text-center blue">BATON ROUGE&#39;S MUSIC CENTER FOR CHILDREN&#38; ADULTS</h5>
-    <h5 class="text-center blue">lessons &#124; recitals &#124; studio classes &#124; summer camps</h5>
+    <h5 class="text-center black">BATON ROUGE&#39;S MUSIC CENTER FOR CHILDREN&#38; ADULTS</h5>
+    <h5 class="text-center black">lessons &#124; recitals &#124; studio classes &#124; summer camps</h5>
     <br>
     <br>
 </div>
--- includes/header.php
+++ includes/header.php
@@ -2,9 +2,11 @@
     <div class="row">
         <div>
             <br>
-            <a href="/">
-                <img class="logo" src="/images/logo2.png">
-            </a>
+            <div class="logo-wrap">
+                <a href="/" class="logo">
+                    <img src="/images/logo-black.png">
+                </a>
+            </div>
             <h6 class="text-center black textshadownl">MUSIC CENTER FOR CHILDREN&#38; ADULTS</h6>
             <br>
         </div> 
