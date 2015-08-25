7f424fcbfcae1f771bbd3bb06670d10130196777
Standard IP <rhroyston@gmail.com>
2015-08-25 19:32:28 +0000

    My changes

--- includes/header-min.php
+++ includes/header-min.php
@@ -5,7 +5,7 @@
         </a>
     </div>
     <div class="row">
-      <h6 class="text-center black adjustup">9270 Siegen Lane #304 Baton Rouge, LA 70810 &#40;225&#41; 767&#45;0030</h6>
+      <h6 class="text-center black">9270 Siegen Lane #304 Baton Rouge, LA 70810 &#40;225&#41; 767&#45;0030</h6>
     </div>
     <h5 class="text-center black">BATON ROUGE&#39;S MUSIC CENTER FOR CHILDREN&#38; ADULTS</h5>
     <h5 class="text-center black">lessons &#124; recitals &#124; studio classes &#124; summer camps</h5>
