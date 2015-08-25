a1fcc8bd0eeda7875072cf3fb5d1a2910eb4726b
Standard IP <rhroyston@gmail.com>
2015-08-25 05:06:23 +0000

    My changes

--- index.php
+++ index.php
@@ -6,8 +6,9 @@
     $title = 'Login';
     include 'includes/head.php';
   ?>
+  <div id="cover"></div>
   <body style='background-image: url("../images/brickwallxl.png");'>
-    <div id="cover"></div>
+    
     <?php include 'includes/socialnav.php';?>
     <?php include 'includes/alert.php';?>
     <?php include 'includes/header.php';?>
