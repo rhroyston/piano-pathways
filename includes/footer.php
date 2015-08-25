0051e43d7eb926de5d0af9c14a0b057c056ee249
Standard IP <rhroyston@gmail.com>
2015-08-25 04:17:24 +0000

    My changes

--- includes/footer.php
+++ includes/footer.php
@@ -14,6 +14,11 @@
     <h3 class="text-center"><a class="black textshadowsm" href="https://www.google.com/maps/place/9270+Siegen+Ln+%23304,+Baton+Rouge,+LA+70810/@30.3672887,-91.0751662,17z/data=!3m1!4b1!4m2!3m1!1s0x8626a53e88fc2d09:0xda2e043d4fe40d5d" target="blank"><i class="fa fa-map-marker"></i> 9270 Siegen Lane #304 Baton Rouge, LA 70810</a></h3>
     <h3 class="text-center"><a class="black textshadowsm" href="tel:225-767-0030"><i class="fa fa-phone"></i> &#40;225&#41; 767&#45;0030</a></h3>
   <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
   <div class="container">
     <div class="row">
       <div class="col-md-2">
@@ -35,12 +40,6 @@
       </div>      
     </div>
   </div>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-
   <br>
   <br>
   <br>
