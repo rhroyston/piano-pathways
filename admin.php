d2e8d5149ea981ff63182a1f708dc482dd08fe08
Standard IP <rhroyston@gmail.com>
2015-08-25 20:54:39 +0000

    My changes

--- admin.php
+++ admin.php
@@ -1,74 +1,19 @@
-<?php
+<?php include 'includes/session_members.php'; ?>
 
-/*** begin the session ***/
-session_start();
-
-if(!isset($_SESSION['user_id']))
-{
-    $message = 'You must be logged in to access this page';
-}
-else
-{
-    try
-    {
-        /*** connect to database ***/
-        /*** mysql hostname ***/
-        $mysql_hostname = '127.8.99.130';
-
-        /*** mysql username ***/
-        $mysql_username = 'adminRqmldJy';
-
-        /*** mysql password ***/
-        $mysql_password = 'gQDlAVx3a66L';
-
-        /*** database name ***/
-        $mysql_dbname = 'thepianopathway';
+<!DOCTYPE html>
+<html lang="en">
+  
+<div id="cover"></div>  
+  
+    <?php 
+        $title = 'Admin';
+        include 'includes/head.php';
+    ?>
+  
+    <body style='background-image: url("../images/fabric.png");'>
+      
 
 
-        /*** select the users name from the database ***/
-        $dbh = new PDO("mysql:host=$mysql_hostname;dbname=$mysql_dbname", $mysql_username, $mysql_password);
-        /*** $message = a message saying we have connected ***/
-
-        /*** set the error mode to excptions ***/
-        $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-        /*** prepare the insert ***/
-        $stmt = $dbh->prepare("SELECT phpro_username FROM phpro_users 
-        WHERE phpro_user_id = :phpro_user_id");
-
-        /*** bind the parameters ***/
-        $stmt->bindParam(':phpro_user_id', $_SESSION['user_id'], PDO::PARAM_INT);
-
-        /*** execute the prepared statement ***/
-        $stmt->execute();
-
-        /*** check for a result ***/
-        $phpro_username = $stmt->fetchColumn();
-
-        /*** if we have no something is wrong ***/
-        if($phpro_username == false)
-        {
-            $message = 'Access Error';
-        }
-        else
-        {
-            $message = 'Welcome '.$phpro_username;
-        }
-    }
-    catch (Exception $e)
-    {
-        /*** if we are here, something is wrong in the database ***/
-        $message = 'We are unable to process your request. Please try again later"';
-    }
-}
-
-?>
-
-<html>
-<head>
-<title>Members Only Page</title>
-</head>
-<body>
-<h2><?php echo $message; ?></h2>
-</body>
+        <h2><?php echo $message; ?></h2>
+    </body>
 </html>
