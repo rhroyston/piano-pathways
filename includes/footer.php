c1757a2745d658eda2e1990da382e3ccda62af51
Standard IP <rhroyston@gmail.com>
2015-08-25 19:11:42 +0000

    My changes

--- includes/footer.php
+++ includes/footer.php
@@ -15,7 +15,7 @@
   <br>
   <br>
   <br>
-  <div class="container whiteback">
+  <div class="container">
     <div class="row">
       <div class="col-sm-4 col-sm-offset-2">
         <h3 class="black textshadownl">Piano Pathways Resources</h3>
@@ -42,29 +42,23 @@
       </div>
       <br>      
     </div>
-  
-  
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-    <h3 class="text-center"><a class="black textshadowsm" href="https://www.google.com/maps/place/9270+Siegen+Ln+%23304,+Baton+Rouge,+LA+70810/@30.3672887,-91.0751662,17z/data=!3m1!4b1!4m2!3m1!1s0x8626a53e88fc2d09:0xda2e043d4fe40d5d" target="blank"><i class="fa fa-map-marker"></i> 9270 Siegen Lane #304 Baton Rouge, LA 70810</a></h3>
-    <h3 class="text-center"><a class="black textshadowsm" href="tel:225-767-0030"><i class="fa fa-phone"></i> &#40;225&#41; 767&#45;0030</a></h3>
-  <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+      <h3 class="text-center"><a class="black textshadowsm" href="https://www.google.com/maps/place/9270+Siegen+Ln+%23304,+Baton+Rouge,+LA+70810/@30.3672887,-91.0751662,17z/data=!3m1!4b1!4m2!3m1!1s0x8626a53e88fc2d09:0xda2e043d4fe40d5d" target="blank"><i class="fa fa-map-marker"></i> 9270 Siegen Lane #304 Baton Rouge, LA 70810</a></h3>
+      <h3 class="text-center"><a class="black textshadowsm" href="tel:225-767-0030"><i class="fa fa-phone"></i> &#40;225&#41; 767&#45;0030</a></h3>
+    <br>
   </div>
   <br>
-  
-  <div class="col-md-1"></div>
   <div class="row">
-    <div class="col-md-10">
+    <div class="col-md-8 col-sm-offset-4">
       <h6 class="text-center"><small class="black">&#169; Copyright 2011&#45;2016 &#124; Piano Pathways &#124; 9270 Siegen Lane #304 &#124; Baton Rouge&#44; LA 70810</small></h6>
     </div>
-  </div>
-  <div class="col-md-1"></div>
-  <br>
-  <br>
-  <br>
+    <br>
+    <br>
+    <br>
   </div>
 </div>
