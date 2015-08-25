0aab23b19df336de15964105edfc8db8f13d316d
Standard IP <rhroyston@gmail.com>
2015-08-25 04:29:24 +0000

    My changes

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     include 'includes/head.php';
   ?>
   <body style='background-image: url("../images/brickwallxl.png");'>
+    <div id="cover"></div>
     <?php include 'includes/socialnav.php';?>
     <?php include 'includes/alert.php';?>
     <?php include 'includes/header.php';?>
--- js/myjs.js
+++ js/myjs.js
@@ -56,6 +56,10 @@ $(document).ready(function () {
   
   
 });
+$(window).load(function(){
+    $('#cover').fadeOut(1000);
+    
+});
 
 
 
