f55294f53aaae7ba2dba0b4491cc0176a88cf1aa
Standard IP <rhroyston@gmail.com>
2015-08-26 00:08:42 +0000

    My changes

--- includes/dbconfig.php
+++ includes/dbconfig.php
@@ -1,3 +1,4 @@
+<?php
     /*** connect to database ***/
     /*** mysql hostname ***/
     $mysql_hostname = '127.8.99.130';
@@ -10,3 +11,4 @@
 
     /*** database name ***/
     $mysql_dbname = 'thepianopathway';
+?>
