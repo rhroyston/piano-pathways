c525121f8339c6317cd0febc6ba7dd516a8fbfb3
Standard IP <rhroyston@gmail.com>
2015-08-27 03:46:18 +0000

    My changes

--- js/myjs.js
+++ js/myjs.js
@@ -65,7 +65,9 @@ $(window).load(function(){
 
   
 $('#myModal').on('hidden.bs.modal', function (e) {
-  $('.modal-content').remove();
+  $('.modal-header').remove();
+  $('.modal-body').remove();
+  $('.modal-footer').remove();
 });
   
 });
