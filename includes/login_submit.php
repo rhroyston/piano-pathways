d5e7bb0aa1b069cdfc73e946ec31365bef0f608c
Standard IP <rhroyston@gmail.com>
2015-08-27 23:12:22 +0000

    My changes

--- includes/login_submit.php
+++ includes/login_submit.php
@@ -106,7 +106,7 @@ else
 // if the user successfully logged in redirect them to original page else redirect them to login page w error reason
 
     if ($message == 'Success: You are now logged in'){
-        if($phpro_username == 'admin'){
+        if($phpro_username === 'admin'){
             header("Location: http://thepianopathway-rhroyston.rhcloud.com/admin");
         }
         //$url = $_SESSION["original-page"];
