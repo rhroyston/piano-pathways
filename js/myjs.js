e627de0c88d637299032f5d8e53ad516b44d2ded
Standard IP <rhroyston@gmail.com>
2015-08-27 03:57:56 +0000

    My changes

--- js/myjs.js
+++ js/myjs.js
@@ -65,7 +65,7 @@ $(window).load(function(){
 
   
 $('#myModal').on('hidden.bs.modal', function (e) {
-  $('.modal-content').empty();
+  //$('.modal-content').empty();
   //$('.modal-header').empty();
   //$('.modal-body').empty();
   //$('.modal-footer').empty();
