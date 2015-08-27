a6e835446bb709e81bc18cebdcab8258a5767866
Standard IP <rhroyston@gmail.com>
2015-08-27 21:08:03 +0000

    My changes

--- local/dbselectuser.1.php
+++ local/dbselectuser.1.php
@@ -1,8 +1,12 @@
+<?php include 'includes/session_members.php'; ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
 		<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+		
 </head>
+
                 <?php
                     $id = $_GET['id'];
                     
