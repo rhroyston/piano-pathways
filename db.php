49a91f375f9c52bb0712ebd22e779f0a224aa6f4
Standard IP <rhroyston@gmail.com>
2015-08-27 00:14:12 +0000

    My changes

--- db.php
+++ db.php
@@ -25,7 +25,7 @@
                              
                     try {
                         $conn = new PDO("mysql:host=$mysql_hostname;dbname=$mysql_dbname", $mysql_username, $mysql_password);
-                        $sql = "SELECT * FROM phpro_users WHERE phpro_users = $id";
+                        $sql = "SELECT * FROM phpro_users WHERE phpro_user_id = $id";
                          $q = $conn->query($sql);
                          $q->setFetchMode(PDO::FETCH_ASSOC);                        
                     } catch (PDOException $pe) {
@@ -33,39 +33,39 @@
                     }                                
                 ?>
 
-<body>
-  <div class="modal-header">
-    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-     <h4 class="modal-title">Modal title</h4>
-
-</div>
-<div class="modal-body">
-
-                <table class="table">
-                    <tr>
-                        <th> </th>
-                        <th>First Name</th>
-                        <th>Last Name</th>
-                        <th>Email</th>
-                        <th>Telephone</th>
-                    </tr>
-                    <?php while ($r = $q->fetch()): ?>
-                    <?php $id = "#id" . htmlspecialchars($r['phpro_user_id']);?> 
-                    <tr>
-                        <a data-toggle="modal" href="<?php echo 'http://thepianopathway-rhroyston.rhcloud.com/db?id=' . htmlspecialchars($r['phpro_user_id']); ?>" data-target="<?php echo '#modal' . htmlspecialchars($r['phpro_user_id'])?>"><i class='fa fa-search-plus'></i></a>
-                        <td><?php echo htmlspecialchars($r['phpro_lastname'])?></td>
-                        <td><?php echo htmlspecialchars($r['phpro_firstname']); ?></td>
-                        <td><?php echo htmlspecialchars($r['phpro_email']); ?></td>
-                        <td><?php echo htmlspecialchars($r['phpro_telephone']); ?></td>
-                    </tr>
-                    <?php endwhile; ?>
-                </table>
-
-</div>
-<div class="modal-footer">
-    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-    <button type="button" class="btn btn-primary">Save changes</button>
-</div>
+                <body>
+                  <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                     <h4 class="modal-title">Modal title</h4>
+                
+                </div>
+                <div class="modal-body">
+                
+                                <table class="table">
+                                    <tr>
+                                        <th> </th>
+                                        <th>First Name</th>
+                                        <th>Last Name</th>
+                                        <th>Email</th>
+                                        <th>Telephone</th>
+                                    </tr>
+                                    <?php while ($r = $q->fetch()): ?>
+                                    <?php $id = "#id" . htmlspecialchars($r['phpro_user_id']);?> 
+                                    <tr>
+                                        <a data-toggle="modal" href="<?php echo 'http://thepianopathway-rhroyston.rhcloud.com/db?id=' . htmlspecialchars($r['phpro_user_id']); ?>" data-target="<?php echo '#modal' . htmlspecialchars($r['phpro_user_id'])?>"><i class='fa fa-search-plus'></i></a>
+                                        <td><?php echo htmlspecialchars($r['phpro_lastname'])?></td>
+                                        <td><?php echo htmlspecialchars($r['phpro_firstname']); ?></td>
+                                        <td><?php echo htmlspecialchars($r['phpro_email']); ?></td>
+                                        <td><?php echo htmlspecialchars($r['phpro_telephone']); ?></td>
+                                    </tr>
+                                    <?php endwhile; ?>
+                                </table>
+                
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
   
 </body>
 </html>
