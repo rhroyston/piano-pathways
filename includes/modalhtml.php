92ed881485b34f195a3a5ea39453afd4500f559f
Standard IP <rhroyston@gmail.com>
2015-08-27 04:13:44 +0000

    My changes

---
+++ includes/modalhtml.php
@@ -0,0 +1,7 @@
+    <div id="myModal" class="modal fade">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <!-- Content will be loaded here from "remote.php" file -->
+            </div>
+        </div>
+    </div>
