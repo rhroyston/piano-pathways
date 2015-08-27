c96713f718a00c47a2cbc92c398cede0de001e8f
Standard IP <rhroyston@gmail.com>
2015-08-27 20:43:36 +0000

    My changes

--- test.php
+++ test.php
@@ -1,11 +1,5 @@
-<?php include 'includes/session.php';?>
-
 <!DOCTYPE html>
 <html lang="en">
-  <?php 
-    $title = 'Test';
-    include 'includes/head1.php';
-  ?>
   <body>
     <?php include '../local/modalhtml.php';?>
   </body>
