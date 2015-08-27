ba590414ed6e2e15d6c63c19d730a926b42f0353
Standard IP <rhroyston@gmail.com>
2015-08-27 02:14:56 +0000

    My changes

--- admin.php
+++ admin.php
@@ -59,12 +59,6 @@
                 </table>
             </div>
         </div>
-        
-        
-        
-        
-        
-        
     </div>
 
 <!-- Modal -->
@@ -72,7 +66,7 @@
     <div class="modal-dialog">
       <div class="modal-content">
         <div class="modal-header">
-          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
           <h4 class="modal-title">Modal title</h4>
         </div>
         <div class="modal-body">
--- db.php
+++ db.php
@@ -42,7 +42,7 @@
 
                 <body>
                   <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
                      <h4 class="modal-title"><?php echo $firstname . " " . $lastname; ?></h4>
                 
                 </div>
