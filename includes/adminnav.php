e7686e35b225392c0d1ceb00ff77d0e5da19b298
Standard IP <rhroyston@gmail.com>
2015-08-28 21:23:51 +0000

    My changes

--- includes/adminnav.php
+++ includes/adminnav.php
@@ -1,16 +1,14 @@
     <h4>
-        &nbsp;
+        
         <a class="black textshadow" href="/"><i class="fa fa-home"></i> Home</a>
           <?php
               if(!isset($_SESSION['user_id']))
               {
-                echo '<span><a href="/login" class="black textshadow pull-right"><i class="fa fa-sign-in"></i> login</a></span>';
+                echo '&nbsp;&nbsp;<span><a href="/login" class="black textshadow pull-right"><i class="fa fa-sign-in"></i> login</a></span>';
               }
               else
               {
-                echo '<span><a href="/logout" class="black textshadow pull-right"> <i class="fa fa-sign-in"></i> logout</a></span>';
+                echo '<span><a href="/logout" class="black textshadow pull-right"> <i class="fa fa-sign-in"></i> logout</a></span>&nbsp;&nbsp;';
               }
             ?>
-        &nbsp;
     </h4>
-    
