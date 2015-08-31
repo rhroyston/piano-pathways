dff35f92317f4fdaa2717959bfd5f467ee0076f3
Standard IP <rhroyston@gmail.com>
2015-08-31 02:48:05 +0000

    My changes

--- includes/newevent.php
+++ includes/newevent.php
@@ -14,12 +14,14 @@ if(!isset($_SESSION['user_id']))
     		<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
     </head>
     <body>
+        <form action="includes/addevent_submit" method="post" data-toggle="validator" id="newevent">
         <div class="modal-header">
             <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
             <h4 class="modal-title">Add New Event</h4>
         </div>
         <div class="modal-body">
-        <form action="includes/addevent_submit" method="post" data-toggle="validator" id="newevent">
+        
+        
             <div class="row">
                 <div class="form-group">
                     <div class="col-sm-10">
@@ -93,11 +95,12 @@ if(!isset($_SESSION['user_id']))
                     </div>
                 </div>
             </div>
-        </form>
+        
         <div class="modal-footer">
             <button type="button" class="btn btn-default" data-dismiss="modal"><i class="fa fa-times"></i> Cancel</button>
             
             <input type="submit" class="btn btn-default" value="Submit" />
         </div>
+        </form>
     </body>
 </html>
