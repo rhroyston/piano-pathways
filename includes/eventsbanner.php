e570a1bc1e52a97665cd989a2a4592b7283b6b98
Standard IP <rhroyston@gmail.com>
2015-09-03 20:28:39 +0000

    My changes

--- includes/eventsbanner.php
+++ includes/eventsbanner.php
@@ -26,8 +26,10 @@ echo '<div class="row">';
 
     while ($r = $q->fetch()):
     $date = date_create_from_format('Y-m-d H:i:s', $r['event_time']);
-    echo htmlspecialchars($r['event_title']) . " ";
-    echo date_format($date, 'l\, F jS h:i A') . "<br>";
+    echo "<h4><i class="fa fa-music"></i> " . htmlspecialchars($r['event_title']) . " " . date_format($date, 'l\, F jS h:i A') . "</h4><br>";
+    echo "<h6>" . $r['event_duration'] . "</H6>";
+    echo "<h6>" . $r['event_street'] . $r['event_city'] . $r['event_state'] . $r['event_zip'] . "</H6>";
+    echo "<h5>" . $r['event_detail'] . "</H5>";
     endwhile;
 
   echo '</div>';
