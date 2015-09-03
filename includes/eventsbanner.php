ac289ba1547acac3da0c2d3b602e63764df82880
Standard IP <rhroyston@gmail.com>
2015-09-03 22:27:18 +0000

    My changes

--- includes/eventsbanner.php
+++ includes/eventsbanner.php
@@ -20,19 +20,19 @@ try {
 } catch (PDOException $pe) {
   die("Could not connect to the database $dbname :" . $pe->getMessage());
 }
-
-echo '<div class="row">';
-  echo '<div class="col-sm-6 col-sm-offset-3">';
-
-    while ($r = $q->fetch()):
-      $date = date_create_from_format('Y-m-d H:i:s', $r['event_time']);
-      echo "<h4><small><i class='fa fa-music'></i></small> " . htmlspecialchars($r['event_title']) . " " . date_format($date, 'l\, F jS h:i A') . "</h4>";
-      echo "<h6>" . $r['event_street'] . " " . $r['event_city'] . ", " . $r['event_state'] . " " . $r['event_zip'] . "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Duration&#58; " . $r['event_duration'] . " Hours</h6>";
-      echo "<h5>" . $r['event_detail'] . "</h5>";
-    endwhile;
-
+echo '<div class="container">';
+  echo '<div class="row">';
+    echo '<div class="col-sm-6 col-sm-offset-3">';
+  
+      while ($r = $q->fetch()):
+        $date = date_create_from_format('Y-m-d H:i:s', $r['event_time']);
+        echo "<h4><small><i class='fa fa-music'></i></small> " . htmlspecialchars($r['event_title']) . " " . date_format($date, 'l\, F jS h:i A') . "</h4>";
+        echo "<h6>" . $r['event_street'] . " " . $r['event_city'] . ", " . $r['event_state'] . " " . $r['event_zip'] . "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Duration&#58; " . $r['event_duration'] . " Hours</h6>";
+        echo "<h5>" . $r['event_detail'] . "</h5>";
+      endwhile;
+  
+    echo '</div>';
   echo '</div>';
 echo '</div>';
-
 
 ?>
