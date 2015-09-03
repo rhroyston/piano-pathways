01f707f82a0274bde0b6642bf6982131bd1d08d2
Standard IP <rhroyston@gmail.com>
2015-09-03 22:38:23 +0000

    My changes

--- includes/eventsbanner.php
+++ includes/eventsbanner.php
@@ -22,7 +22,7 @@ try {
 }
 echo '<div class="container">';
   echo '<div class="row">';
-    echo '<div class="col-sm-6 col-sm-offset-3">';
+    echo '<div class="col-sm-6 col-sm-offset-3 events">';
   
       while ($r = $q->fetch()):
         $date = date_create_from_format('Y-m-d H:i:s', $r['event_time']);
