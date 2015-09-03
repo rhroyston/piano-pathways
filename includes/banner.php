d344fec70986200d8089840071e9caab72c6a7e4
Standard IP <rhroyston@gmail.com>
2015-09-03 04:36:16 +0000

    My changes

--- includes/banner.php
+++ includes/banner.php
@@ -4,18 +4,22 @@
     <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
     <li data-target="#carousel-example-generic" data-slide-to="1"></li>
     <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="3"></li>
   </ol>
 
   <!-- Wrapper for slides -->
   <div class="carousel-inner" role="listbox">
     <div class="item active">
-      <img src="/images/studio.jpg" alt="...">
+      <img src="/images/c1.jpg" alt="...">
     </div>
     <div class="item">
-      <img src="/images/children.jpg" alt="...">
+      <img src="/images/c2.jpg" alt="...">
     </div>
     <div class="item">
-      <img src="/images/adult.jpg" alt="...">
+      <img src="/images/c3.jpg" alt="...">
+    </div>
+    <div class="item">
+      <img src="/images/c4.jpg" alt="...">
     </div>
   </div>
 
