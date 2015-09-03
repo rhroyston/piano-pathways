966e4b2b3536927a6ac6a6bd953f71c0f0ded2a6
Standard IP <rhroyston@gmail.com>
2015-09-03 00:57:48 +0000

    My changes

--- logout.php
+++ logout.php
@@ -2,13 +2,11 @@
 // Begin the session
 session_start();
 
-$url = $_SERVER['HTTP_REFERER'];
-
     session_unset();
     session_destroy();
     session_write_close();
     setcookie(session_name(),'',0,'/');
     session_regenerate_id(true);
 
-header("Location: $url");
+header("Location: /");
 ?>
