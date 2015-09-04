6038ef958aa3d653cf7ce2229aad3e141a3fbedd
Standard IP <rhroyston@gmail.com>
2015-09-04 02:59:36 +0000

    My changes

--- policy.php
+++ policy.php
@@ -65,12 +65,10 @@
             <br>
             <h4><i class="fa fa-exclamation-circle"></i> Please note these policies are put in place to protect the interest of the instructors and piano students. This studio policy serves as a formal document for service of and payment for piano instruction. Registering for lessons indicates that the student and parents have read&#44; agree to&#44; and will comply with all policies outlined.
             </h4>
+            <br>
+            <br>
             <?php include 'includes/copyright.php';?>
-            
         </div>
         <br>
-        <br>
-        <br>
-        
     </body>
 </html>
