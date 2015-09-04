cc40dbcf893e0ea95d548dd1002eeaffdbb592e2
Standard IP <rhroyston@gmail.com>
2015-09-04 02:53:00 +0000

    My changes

--- includes/copyright.php
+++ includes/copyright.php
@@ -1,4 +1,4 @@
 
-<div class="col-md-6 col-sm-offset-3">
+
     <h6 class="text-center"><small class="black">&#169; Copyright 2011&#45;2016 &#124; Piano Pathways &#124; 9270 Siegen Lane #304 &#124; Baton Rouge&#44; LA 70810</small></h6>
-</div>
+
