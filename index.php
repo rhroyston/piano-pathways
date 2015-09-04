5ace66fe2d365eb28643b641e5cb94d4ef235263
Standard IP <rhroyston@gmail.com>
2015-09-04 01:44:14 +0000

    My changes

--- index.php
+++ index.php
@@ -23,17 +23,17 @@
     <br>
     <div class="container">
       <div class="row text-center">
-        <h1>
+
         <div class="col-md-2 col-md-offset-3">
-          <a class="black textshadow" href="about">Who We Are</a>
+          <a class="custombotton black textshadow" href="about">Who We Are</a>
         </div>    
         <div class="col-md-2">
-          <a class="black textshadow" href="instructors">Our Instructors</a>
+          <a class="custombotton black textshadow" href="instructors">Our Instructors</a>
         </div>    
         <div class="col-md-2">
-          <a class="black textshadow" href="studio">View The Studio</a>
+          <a class="custombotton black textshadow" href="studio">View The Studio</a>
         </div>    
-        </h1>
+
       </div>
     </div>
     <?php
