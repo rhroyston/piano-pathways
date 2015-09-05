19d148596ecaaac2c657c0d11c2977b6afb3098f
Standard IP <rhroyston@gmail.com>
2015-09-05 18:51:47 +0000

    My changes

--- index.php
+++ index.php
@@ -30,7 +30,7 @@
         <br>
         <br>
       </div>
-      div class="row text-center">
+      <div class="row text-center">
         <div class="col-sm-6 col-sm-offset-3">
           <h5 class="text-center"><a class="black textshadowsm" href="https://www.google.com/maps/place/9270+Siegen+Ln+%23304,+Baton+Rouge,+LA+70810/@30.3672887,-91.0751662,17z/data=!3m1!4b1!4m2!3m1!1s0x8626a53e88fc2d09:0xda2e043d4fe40d5d" target="blank"><i class="fa fa-map-marker"></i> 9270 Siegen Lane #304 Baton Rouge, LA 70810</a></h5>
           <h5 class="text-center"><a class="black textshadowsm" href="tel:225-767-0030"><i class="fa fa-phone"></i> &#40;225&#41; 767&#45;0030</a></h5>
