b674f242ad7b7ac9ffdf93487bd16a21d4d9866c
Standard IP <rhroyston@gmail.com>
2015-09-05 16:23:04 +0000

    My changes

--- includes/socialnav.php
+++ includes/socialnav.php
@@ -1,8 +1,5 @@
     <h4 class="text-right">
         <!-- Button trigger modal -->
-        <a class="black textshadow" href="https://twitter.com/PianoPathwaysBR"><i class="fa fa-twitter"></i></a>&nbsp;&nbsp;
-        <a class="black textshadow" href="https://plus.google.com/114075402524593080864/posts"><i class="fa fa-google-plus"></i></a>&nbsp;&nbsp;
-        <a class="black textshadow" href="https://www.linkedin.com/pub/rebecca-bellelo/39/4a6/73"><i class="fa fa-linkedin"></i></a>&nbsp;&nbsp;
           <?php
               if(!isset($_SESSION['user_id']))
               {
--- index.php
+++ index.php
@@ -18,9 +18,9 @@
     <div class="container">
       <div class="row text-center">
         <div class="col-sm-8 col-sm-offset-2">
-          <a class="custombotton" href="https://www.facebook.com/PianoPathwaysStudio" target="_blank"><i class="fa fa-facebook-square"></i> Follow Us</a>
+          <a class="custombotton" href="https://www.facebook.com/PianoPathwaysStudio" target="_blank"><i class="fa fa-facebook-square"></i> Friend Us</a>
           <a class="custombotton" href="instructors"><i class="fa fa-users"></i> Our Instructors</a>
-          <a class="custombotton" href="studio"><i class="fa fa-music"></i> View The Studio</a>
+          <a class="custombotton" href="https://twitter.com/PianoPathwaysBR" target="_blank"><i class="fa fa-twitter"></i> Follow Us</a>
           <a class="custombotton" href="https://www.youtube.com/user/rbellelo" target="_blank"><i class="fa fa-youtube"></i> Check Us Out</a>
         </div>    
       </div>
