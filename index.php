e45be10223e4e5c19367167c7a07dc0250a2be54
Standard IP <rhroyston@gmail.com>
2015-09-05 18:02:05 +0000

    My changes

--- index.php
+++ index.php
@@ -17,16 +17,22 @@
     <?php include 'includes/header.php';?>
     <div class="container">
       <div class="row text-center">
-        <div class="col-sm-8 col-sm-offset-2">
+        <div class="col-sm-2 col-sm-offset-4">
           <a class="custombotton" href="https://www.facebook.com/PianoPathwaysStudio" target="_blank"><i class="fa fa-facebook-square"></i> Friend Us</a>
+        </div>
+        <div class="col-sm-2">
           <a class="custombotton" href="instructors"><i class="fa fa-users"></i> Our Instructors</a>
+        </div>
+        <div class="col-sm-2">
           <a class="custombotton" href="https://twitter.com/PianoPathwaysBR" target="_blank"><i class="fa fa-twitter"></i> Follow Us</a>
+        </div>
+        <div class="col-sm-2">
           <a class="custombotton" href="https://www.youtube.com/user/rbellelo" target="_blank"><i class="fa fa-youtube"></i> Check Us Out</a>
-          <br>
-          <br>
-    <h5 class="text-center"><a class="black textshadowsm" href="https://www.google.com/maps/place/9270+Siegen+Ln+%23304,+Baton+Rouge,+LA+70810/@30.3672887,-91.0751662,17z/data=!3m1!4b1!4m2!3m1!1s0x8626a53e88fc2d09:0xda2e043d4fe40d5d" target="blank"><i class="fa fa-map-marker"></i> 9270 Siegen Lane #304 Baton Rouge, LA 70810</a></h5>
-    <h5 class="text-center"><a class="black textshadowsm" href="tel:225-767-0030"><i class="fa fa-phone"></i> &#40;225&#41; 767&#45;0030</a></h5>
-        </div>    
+        </div>
+        <br>
+        <br>
+        <h5 class="text-center"><a class="black textshadowsm" href="https://www.google.com/maps/place/9270+Siegen+Ln+%23304,+Baton+Rouge,+LA+70810/@30.3672887,-91.0751662,17z/data=!3m1!4b1!4m2!3m1!1s0x8626a53e88fc2d09:0xda2e043d4fe40d5d" target="blank"><i class="fa fa-map-marker"></i> 9270 Siegen Lane #304 Baton Rouge, LA 70810</a></h5>
+        <h5 class="text-center"><a class="black textshadowsm" href="tel:225-767-0030"><i class="fa fa-phone"></i> &#40;225&#41; 767&#45;0030</a></h5>
       </div>
     </div>
     <br>
