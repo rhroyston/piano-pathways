dcdcc6244efe4ae37dd8865f502628a94fb3a58e
Standard IP <rhroyston@gmail.com>
2015-09-05 22:51:48 +0000

    My changes

--- includes/eventsbanner.php
+++ includes/eventsbanner.php
@@ -29,7 +29,7 @@ try {
             echo '<div class="num-day">' . date_format($date, 'j') . '</div>';
           echo '</div>';
         
-			    echo '<div class="text-left">';
+			    echo '<div class="event-text text-left">';
           
             echo "<span class='black event-title'>" . htmlspecialchars($r['event_title']) . "</span>";
             echo "<h6 class='black'>" . $r['event_street'] . " " . $r['event_city'] . ", " . $r['event_state'] . " " . $r['event_zip'] . "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Duration&#58; " . $r['event_duration'] . " Hours</h6>";
