9969a5cfe2d4536f430fb5bc7aaa5eedd14ab8af
Standard IP <rhroyston@gmail.com>
2015-09-05 21:18:32 +0000

    My changes

--- includes/eventsbanner.php
+++ includes/eventsbanner.php
@@ -20,7 +20,7 @@ try {
 } catch (PDOException $pe) {
   die("Could not connect to the database $dbname :" . $pe->getMessage());
 }
-    echo '<div class="col-sm-4 col-sm-offset-2">';
+    echo '<div class="col-sm-6 col-sm-offset-2">';
     echo '<h2 class="eventheader"><i class="fa fa-calendar-o"></i> Upcoming Events</h2>';
       while ($r = $q->fetch()):
         $date = date_create_from_format('Y-m-d H:i:s', $r['event_time']);
