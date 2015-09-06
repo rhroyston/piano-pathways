c2a0ff72d06ea0089a5a2087fe515ab33bf7b78f
Standard IP <rhroyston@gmail.com>
2015-09-06 05:09:45 +0000

    My changes

--- includes/footer.php
+++ includes/footer.php
@@ -9,7 +9,10 @@
   <br>
   <br>
   <div class="container">
-
+  <br>
+  <br>
+  <br>
+  <br>
     <div class="row">
       <div class="col-sm-4 col-sm-offset-2">
         <span class="foot-big-text">About</span><br>
