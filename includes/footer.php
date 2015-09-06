2bf9cc2d0c7acd2f3ccb29ac777c503c65ff32c5
Standard IP <rhroyston@gmail.com>
2015-09-06 03:39:47 +0000

    My changes

--- includes/footer.php
+++ includes/footer.php
@@ -23,7 +23,7 @@
     <br>
     <div class="row">
       <div class="col-sm-4 col-sm-offset-2">
-        <h3 class="foot">About</h3>
+        <h2 class="foot">About</h2>
         <a class="foot" href="#faqModal" data-toggle="modal"><i class="fa fa-question-circle"></i> FAQ&#39;s <i class="fa fa-angle-double-right"></i></a><br>
         <a class="foot" href="policy"><i class="fa fa-info-circle"></i> Studio Policy</a><br>
         <a class="foot" href="#schedulingModal" data-toggle="modal">Scheduling <i class="fa fa-angle-double-right"></i></a><br>
@@ -31,7 +31,7 @@
         <a class="foot" href="#wheretuitiongoesModal" data-toggle="modal">Where Does Tuition Go&#63; <i class="fa fa-angle-double-right"></i></a><br>
       </div>
       <div class="col-sm-4 col-sm-offset-2">
-        <h3 class="foot">Links</h3>
+        <h2 class="foot">Links</h2>
         <a class="foot" href="http://www.musiclearningcommunity.com" target="_blank">Make A Payment <i class="fa fa-external-link"></i></a><br>
         <a class="foot" href="http://www.encoremusiclessons.com/images/infographic/infographic-piano-lessons-are-good-for-you-and-your-brain.png" target="_blank">Benefits of Piano Lessons <i class="fa fa-external-link"></i></a><br>
         <a class="foot" href="https://www.metronomeonline.com/" target="_blank">Metronome Online <i class="fa fa-external-link"></i></a><br>
