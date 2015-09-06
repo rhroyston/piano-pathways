82b4b6d62d0f162c91708c6a85d07e464aa00ec1
Standard IP <rhroyston@gmail.com>
2015-09-06 01:20:42 +0000

    My changes

--- includes/announcementsbanner.php
+++ includes/announcementsbanner.php
@@ -22,7 +22,7 @@ try {
 }
   
       while ($r = $q->fetch()):
-        echo '<div class="box effect7 col-sm-4">';
+        echo '<div class="box effect2 col-sm-4">';
           echo "<h4 class='black'>" . htmlspecialchars($r['announcement_title']) . "</h4>";
           echo "<h5 class='black'>" . $r['announcement_detail'] . "</h5><br>";
         echo '</div>';
