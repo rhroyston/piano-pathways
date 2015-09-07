4e4872214a14496c0916a4a6589cf47950d9c068
Standard IP <rhroyston@gmail.com>
2015-09-07 19:52:02 +0000

    My changes

--- index.php
+++ index.php
@@ -69,7 +69,7 @@
     <div class="container-fluid">
       <div class="row">
         <?php
-          $footerimage = "leather";
+          $footerimage = "brownpaper";
           include 'includes/footer.php';
           include 'includes/resources.php';
         ?>
