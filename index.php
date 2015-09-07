8deaceb3f8c4e17b43f4f24924f2b94ccf662584
Standard IP <rhroyston@gmail.com>
2015-09-07 01:52:25 +0000

    My changes

--- index.php
+++ index.php
@@ -32,29 +32,39 @@
       <br>
       <br>
       <br>
-      <?php include 'includes/eventsbanner.php';?>
+      <div class="container">
+        <div class="row">
+          <?php include 'includes/eventsbanner.php';?>
+        </div>
+      </div>
+      <div class="container">
+        <div class="row">      
+          <div class="polaroids">
+            <div class="polaroid">
+              <p>The New Studio</p>
+              <img src="images/thestudio.jpg">
+            </div>
+            <div class="polaroid">
+              <p>Childrens Recital</p>
+              <img src="images/thechildren.jpg">
+            </div>
+            <div class="polaroid">
+              <p>Group Lessons</p>
+              <img src="images/theinside.jpg">
+            </div>
+            <div class="polaroid">
+              <p>Halloween Fun!</p>
+              <img src="images/thelearning.jpg">
+            </div>
+          </div>
+        </div>
+      </div>
       
-      <div class="polaroids">
-        <div class="polaroid">
-          <p>The New Studio</p>
-          <img src="images/thestudio.jpg">
+      <div class="container">
+        <div class="row">      
+          <?php include 'includes/announcementsbanner.php';?>
         </div>
-        <div class="polaroid">
-          <p>Childrens Recital</p>
-          <img src="images/thechildren.jpg">
-        </div>
-        <div class="polaroid">
-          <p>Group Lessons</p>
-          <img src="images/theinside.jpg">
-        </div>
-        <div class="polaroid">
-          <p>Halloween Fun!</p>
-          <img src="images/thelearning.jpg">
-        </div>
-      </div>      
-      
-      
-      <?php include 'includes/announcementsbanner.php';?>
+      </div>
       <br>
       <br>
 
